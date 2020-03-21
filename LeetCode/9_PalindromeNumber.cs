fb92bbe1c0338079e6547927e748e3197bb56af5
Jim Li <lij42@airproducts.com>
2020-03-21 11:49:21 +0800

    Palindrome Number

---
+++ LeetCode/9_PalindromeNumber.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Algorithym.LeetCode
+{
+    public static class PalindromeNumber
+    {
+        public static bool IsPalindrome(int x)
+        {
+            if (x < 0)
+                return false;
+            if (x < 10)
+                return true;
+
+            string str = x.ToString();
+            int i = 0, j = str.Length - 1;
+            while (j > i + 1)
+            {
+                if (str[i] != str[j])
+                    return false;
+
+                i++;
+                j--;
+            }
+            return i == j ? true : (str[i] == str[j]);
+        }
+    }
+}
