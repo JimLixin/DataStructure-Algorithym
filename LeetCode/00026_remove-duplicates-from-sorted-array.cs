210c2c2f06060ae6a5881f60fec81912aa5278b2
Jim Li <lij42@airproducts.com>
2020-03-28 16:47:20 +0800

    26.remove_duplicates_from_sorted_array

---
+++ LeetCode/00026_remove-duplicates-from-sorted-array.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Algorithym.LeetCode
+{
+    public static class remove_duplicates_from_sorted_array
+    {
+        public static int RemoveDuplicates(int[] nums)
+        {
+            if (nums == null || nums.Length <= 0)
+                return 0;
+            int bufStart = 1, bufEnd = 1, current = nums[0];
+            int n = nums.Length;
+            while (bufEnd < n)
+            {
+                if (current != nums[bufEnd])
+                {
+                    if (bufEnd > bufStart)
+                    {
+                        nums[bufStart] = nums[bufEnd];
+                    }
+                    current = nums[bufEnd];
+                    bufStart++;
+                }
+                bufEnd++;
+            }
+            return bufStart;
+        }
+    }
+}
