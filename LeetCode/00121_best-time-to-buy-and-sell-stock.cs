9b9a69056671c9dc2f1c1bce3b872b6599aef225
Jim Li <lij42@airproducts.com>
2020-04-04 10:31:02 +0800

    121.best_time_to_buy_and_sell_stock

---
+++ LeetCode/00121_best-time-to-buy-and-sell-stock.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Algorithym.LeetCode
+{
+    public static class best_time_to_buy_and_sell_stock
+    {
+        public static int Answer(int[] prices)
+        {
+            if (prices == null || prices.Length == 0)
+                return 0;
+            int len = prices.Length, buy = int.MaxValue, sell = 0;
+
+            for (int i = 0; i < len; i++)
+            {
+                if (prices[i] < buy)
+                    buy = prices[i];
+                else
+                    sell = Math.Max(sell, prices[i] - buy);
+            }
+            return sell;
+        }
+    }
+}
