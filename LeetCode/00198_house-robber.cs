308fae06a1b10a681a28c0b0e6703de203d3bd59
Jim Li <lij42@airproducts.com>
2020-04-06 16:44:05 +0800

    198.house_robber

---
+++ LeetCode/00198_house-robber.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Algorithym.LeetCode
+{
+    public class house_robber
+    {
+        public int Rob(int[] nums)
+        {
+            if (nums == null || nums.Length == 0)
+                return 0;
+            if (nums.Length == 1)
+                return nums[0];
+
+            int len = nums.Length;
+            int[] dp = new int[3];
+            dp[0] = nums[0];
+            dp[1] = Math.Max(nums[0], nums[1]);
+
+            for (int i = 2; i < len; i++)
+            {
+                dp[i % 3] = Math.Max(dp[(i - 2) % 3] + nums[i], dp[(i - 1) % 3]);
+            }
+
+            return dp[(len - 1) % 3];
+        }
+    }
+}
