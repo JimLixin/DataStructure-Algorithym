b180b06f56b0cce8239a808fbfd568d4f0b52efc
Jim Li <lij42@airproducts.com>
2020-04-09 20:45:56 +0800

    67.add_binary

---
+++ LeetCode/00067_add-binary.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Algorithym.LeetCode
+{
+    /// <summary>
+    /// https://leetcode.com/problems/add-binary/
+    /// </summary>
+    public class add_binary
+    {
+        public string AddBinary(string a, string b)
+        {
+            int alen = a.Length, blen = b.Length, len = Math.Max(alen, blen), tmp = 0, flag = 0;
+            if (alen > blen)
+                b = b.PadLeft(alen, '0');
+            else
+                a = a.PadLeft(blen, '0');
+
+            char[] data = new char[len];
+            while (len > 0)
+            {
+                tmp = (a[len - 1] - 48) + (b[len - 1] - 48) + flag;
+                data[len - 1] = (char)(tmp % 2 + 48);
+                flag = tmp / 2;
+                len--;
+            }
+
+            return new String(flag > 0 ? (new char[] { '1' }).Concat(data).ToArray() : data);
+        }
+    }
+}
