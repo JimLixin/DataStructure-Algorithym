0e7fe0d92ba7e9a05e3a1c9ded4ca8d5be52ab61
Jim Li <lij42@airproducts.com>
2020-04-10 20:03:54 +0800

    58.length_of_last_word

---
+++ LeetCode/00058_length-of-last-word.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Algorithym.LeetCode
+{
+    /// <summary>
+    /// https://leetcode.com/problems/length-of-last-word/
+    /// </summary>
+    public class length_of_last_word
+    {
+        public int LengthOfLastWord(string s)
+        {
+            if (string.IsNullOrEmpty(s))
+                return 0;
+            s = s.Trim();
+            int len = s.Length, count = 0;
+            for (int i = len - 1; i >= 0; i--)
+            {
+                if (s[i] == ' ')
+                    return count;
+                count++;
+            }
+            return count;
+        }
+    }
+}
