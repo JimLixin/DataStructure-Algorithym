ee322aaba9905d54b92901e51202f0bba1484a3d
Jim Li <lij42@airproducts.com>
2020-04-12 15:35:49 +0800

    242. Valid Anagram

---
+++ LeetCode/00242_valid-anagram.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Algorithym.LeetCode
+{
+    /// <summary>
+    /// https://leetcode.com/problems/valid-anagram/
+    /// </summary>
+    public class valid_anagram
+    {
+        public bool IsAnagram(string s, string t)
+        {
+            char[] sArr = s.ToCharArray();
+            Array.Sort(sArr);
+            char[] tArr = t.ToCharArray();
+            Array.Sort(tArr);
+            return new String(sArr) == new String(tArr);
+        }
+
+        public bool IsAnagramV2(string s, string t)
+        {
+            return false;
+        }
+    }
+}
