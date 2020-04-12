1c972d7956509c1da99ebb3547859fc6c7c12c63
Jim Li <lij42@airproducts.com>
2020-04-12 10:49:57 +0800

    94. Binary Tree Inorder Traversal

---
+++ LeetCode/00094_binary-tree-inorder-traversal.cs
@@ -0,0 +1,33 @@
+﻿using Algorithym.Shared;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Algorithym.LeetCode
+{
+    /// <summary>
+    /// https://leetcode.com/problems/binary-tree-inorder-traversal/
+    /// </summary>
+    public class binary_tree_inorder_traversal
+    {
+        private IList<int> result;
+        public IList<int> InorderTraversal(TreeNode root)
+        {
+            result = new List<int>();
+            travel(root);
+            return result;
+        }
+
+        public void travel(TreeNode node)
+        {
+            if (node != null)
+            {
+                travel(node.LeftChild);
+                result.Add(node.Value);
+                travel(node.RightChild);
+            }
+        }
+    }
+}
