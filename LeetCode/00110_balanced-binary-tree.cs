90b405a25091998f40b7af2b0d35be5809f9b95a
Jim Li <lij42@airproducts.com>
2020-04-16 21:28:40 +0800

    110. Balanced Binary Tree

---
+++ LeetCode/00110_balanced-binary-tree.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Algorithym.LeetCode
+{
+    /// <summary>
+    /// https://leetcode.com/problems/balanced-binary-tree/
+    /// </summary>
+    public class balanced_binary_tree
+    {
+        public bool IsBalanced(TreeNode root)
+        {
+            if (root == null)
+                return true;
+            return (Math.Abs(getHeight(root.left) - getHeight(root.right)) <= 1) && IsBalanced(root.left) && IsBalanced(root.right);
+        }
+
+        public int getHeight(TreeNode node)
+        {
+            if (node == null)
+                return 0;
+            return 1 + Math.Max(getHeight(node.left), getHeight(node.right));
+        }
+    }
+}
