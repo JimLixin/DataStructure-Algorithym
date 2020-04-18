adbc5999933edb6ea7b9046f4c21312e13699f33
Jim Li <lij42@airproducts.com>
2020-04-18 16:20:16 +0800

    119. Pascal's Triangle II

---
+++ LeetCode/00119_pascals-triangle-ii.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Algorithym.LeetCode
+{
+    /// <summary>
+    /// https://leetcode.com/problems/pascals-triangle-ii/
+    /// </summary>
+    public class pascals_triangle_ii
+    {
+        public IList<int> GetRow(int rowIndex)
+        {
+            rowIndex = rowIndex < 0 ? 1 : rowIndex;
+            int[] prev = null;
+            for (int i = 0; i <= rowIndex; i++)
+            {
+                int[] cur = new int[rowIndex + 1];
+                cur[0] = 1;
+                cur[i] = 1;
+                for (int j = 1; j < i; j++)
+                {
+                    cur[j] = prev[j - 1] + prev[j];
+                }
+                prev = cur;
+            }
+            return prev;
+        }
+    }
+}
