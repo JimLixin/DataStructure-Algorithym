97fb901013ca35e0742caac688561a79173bc49e
Jim Li <lij42@airproducts.com>
2020-04-18 16:07:51 +0800

    118. Pascal's Triangle

---
+++ LeetCode/00118_pascals-triangle.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Algorithym.LeetCode
+{
+    /// <summary>
+    /// https://leetcode.com/problems/pascals-triangle/
+    /// </summary>
+    public class pascals_triangle
+    {
+        public IList<IList<int>> Generate(int numRows)
+        {
+            numRows = numRows < 0 ? 1 : numRows;
+            IList<IList<int>> data = new List<IList<int>>();
+            int[] pre = new int[numRows];
+            for (int i = 0; i < numRows; i++)
+            {
+                data.Add(new int[i + 1]);
+                data[i][0] = 1;
+                data[i][i] = 1;
+                for (int j = 1; j < i; j++)
+                {
+                    data[i][j] = pre[j - 1] + pre[j];
+                }
+                pre = data[i].ToArray();
+            }
+            return data;
+        }
+    }
+}
