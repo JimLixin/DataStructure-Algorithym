e11920ec8ca56069b1339aef7d5bab871f32ab17
Jim Li <lij42@airproducts.com>
2020-04-22 18:35:24 +0800

    985. Sum of Even Numbers After Queries

---
+++ LeetCode/LCOF/00985_sum-of-even-numbers-after-queries.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Algorithym.LeetCode.LCOF
+{
+    /// <summary>
+    /// https://leetcode-cn.com/problems/sum-of-even-numbers-after-queries/
+    /// </summary>
+    public class sum_of_even_numbers_after_queries
+    {
+        public int[] SumEvenAfterQueries(int[] A, int[][] queries)
+        {
+            int len = queries.Length, tmp, incre, index, added, sum = A.Where(a => a % 2 == 0).Sum();
+            int[] data = new int[len];
+            for (int i = 0; i < len; i++)
+            {
+                index = queries[i][1];
+                incre = queries[i][0];
+                tmp = A[index] + incre;
+                added = (A[index] % 2 == 0 ? (tmp % 2 == 0 ? incre : -A[index]) : (tmp % 2 == 0 ? tmp : 0));
+                data[i] = sum + added;
+                sum += added;
+                //Console.WriteLine("A is:" + string.Join(",", A) + $" A[{index}] is {A[index]}, now add by {incre}. Sum will change from {sum} to {data[i]}.");
+                A[index] = tmp;
+            }
+            return data;
+        }
+    }
+}
---
+++ LeetCode/LCOF/shu-zu-zhong-zhong-fu-de-shu-zi-lcof.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Algorithym.LeetCode.LCOF
+{
+    /// <summary>
+    /// https://leetcode-cn.com/problems/shu-zu-zhong-zhong-fu-de-shu-zi-lcof/
+    /// </summary>
+    public class shu_zu_zhong_zhong_fu_de_shu_zi_lcof
+    {
+        /// <summary>
+        /// Good at space complexity
+        /// </summary>
+        /// <param name="nums"></param>
+        /// <returns></returns>
+        public int FindRepeatNumber(int[] nums)
+        {
+            Array.Sort(nums);
+            for (int i = 0; i < nums.Length - 1; i++)
+            {
+                if ((nums[i] ^ nums[i + 1]) == 0)
+                    return nums[i];
+            }
+            return -1;
+        }
+
+        /// <summary>
+        /// Good at time complexity
+        /// </summary>
+        /// <param name="nums"></param>
+        /// <returns></returns>
+        public int FindRepeatNumberV2(int[] nums)
+        {
+            if (nums == null || nums.Length == 0)
+                return -1;
+            int len = nums.Length, cur;
+            int[] data = new int[len];
+            for (int i = 0; i < len; i++)
+            {
+                cur = nums[i];
+                data[cur] = data[cur] > 0 ? (data[cur] + 1) : 1;
+                if (data[cur] > 1)
+                    return cur;
+            }
+            return -1;
+        }
+    }
+}
