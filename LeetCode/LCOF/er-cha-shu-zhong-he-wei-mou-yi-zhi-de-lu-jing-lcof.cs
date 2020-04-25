c6eb1c28f5ace052041ed6cc1dcc7bc5d21b54db
Jim Li <lij42@airproducts.com>
2020-04-25 18:29:50 +0800

    面试题34. 二叉树中和为某一值的路径

---
+++ LeetCode/LCOF/er-cha-shu-zhong-he-wei-mou-yi-zhi-de-lu-jing-lcof.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Algorithym.LeetCode.LCOF
+{
+    /// <summary>
+    /// 面试题34. 二叉树中和为某一值的路径
+    /// https://leetcode-cn.com/problems/er-cha-shu-zhong-he-wei-mou-yi-zhi-de-lu-jing-lcof/
+    /// </summary>
+    public class er_cha_shu_zhong_he_wei_mou_yi_zhi_de_lu_jing_lcof
+    {
+        IList<IList<int>> result;
+        IList<int> data;
+        int step, total;
+        public IList<IList<int>> PathSum(TreeNode root, int sum)
+        {
+            result = new List<IList<int>>();
+            if (root == null)
+                return result;
+            data = new List<int>();
+            travel(root, sum, 0);
+            return result;
+        }
+
+        public void travel(TreeNode node, int sum, int step)
+        {
+            total += node.val;
+            data.Add(node.val);
+            if (node.left == null && node.right == null)
+            {
+                if (total == sum)
+                    result.Add(new List<int>(data));
+                data.RemoveAt(step);
+                total -= node.val;
+                return;
+            }
+            if (node.left != null)
+                travel(node.left, sum, step + 1);
+            if (node.right != null)
+                travel(node.right, sum, step + 1);
+            data.RemoveAt(step);
+            total -= node.val;
+        }
+    }
+}
