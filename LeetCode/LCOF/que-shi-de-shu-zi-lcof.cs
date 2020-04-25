7a2cc8c9175e0f4488b4f93d946fb02d30f5acb7
Jim Li <lij42@airproducts.com>
2020-04-25 19:06:04 +0800

    面试题53 - II. 0～n-1中缺失的数字

---
+++ LeetCode/LCOF/que-shi-de-shu-zi-lcof.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Algorithym.LeetCode.LCOF
+{
+    /// <summary>
+    /// 面试题53 - II. 0～n-1中缺失的数字
+    /// https://leetcode-cn.com/problems/que-shi-de-shu-zi-lcof/
+    /// </summary>
+    public class que_shi_de_shu_zi_lcof
+    {
+        public int MissingNumber(int[] nums)
+        {
+            int xor = 0;
+            for (int i = 0; i < nums.Length; i++)
+            {
+                xor = xor ^ (i + 1) ^ nums[i];
+            }
+            return xor;
+        }
+    }
+}
