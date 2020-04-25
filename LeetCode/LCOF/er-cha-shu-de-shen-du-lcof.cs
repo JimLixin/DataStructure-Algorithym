4e5d51df070cebd9071dad0e7e0bc0b4c3ef9e1f
Jim Li <lij42@airproducts.com>
2020-04-25 18:39:10 +0800

    面试题55 - I. 二叉树的深度

---
+++ LeetCode/LCOF/er-cha-shu-de-shen-du-lcof.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Algorithym.LeetCode.LCOF
+{
+    /// <summary>
+    /// 面试题55 - I. 二叉树的深度
+    /// https://leetcode-cn.com/problems/er-cha-shu-de-shen-du-lcof/
+    /// </summary>
+    public class er_cha_shu_de_shen_du_lcof
+    {
+        public int MaxDepth(TreeNode root)
+        {
+            return root == null ? 0 : (1 + Math.Max(MaxDepth(root.left), MaxDepth(root.right)));
+        }
+    }
+}
