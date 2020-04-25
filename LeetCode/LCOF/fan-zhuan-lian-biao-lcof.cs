1845188c2ab31c9e858c4f23bbaf9ae9defdd83f
Jim Li <lij42@airproducts.com>
2020-04-25 14:21:52 +0800

    面试题24. 反转链表

---
+++ LeetCode/LCOF/fan-zhuan-lian-biao-lcof.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Algorithym.LeetCode.LCOF
+{
+    /// <summary>
+    /// 面试题24. 反转链表
+    /// https://leetcode-cn.com/problems/fan-zhuan-lian-biao-lcof/
+    /// </summary>
+    public class fan_zhuan_lian_biao_lcof
+    {
+        public ListNode ReverseList(ListNode head)
+        {
+            if (head == null)
+                return null;
+            ListNode p1 = null, p2 = head, tmp;
+            while (p2 != null)
+            {
+                tmp = p2.next;
+                p2.next = p1;
+                p1 = p2;
+                p2 = tmp;
+            }
+            return p1;
+        }
+    }
+}
