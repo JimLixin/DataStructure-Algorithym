693fbf393029192543d871096825d0bd409f298e
Jim Li <lij42@airproducts.com>
2020-04-25 11:36:26 +0800

    面试题18. 删除链表的节点

---
+++ LeetCode/LCOF/shan-chu-lian-biao-de-jie-dian-lcof.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Algorithym.LeetCode.LCOF
+{
+    /// <summary>
+    /// 面试题18. 删除链表的节点
+    /// https://leetcode-cn.com/problems/shan-chu-lian-biao-de-jie-dian-lcof/
+    /// </summary>
+    public class shan_chu_lian_biao_de_jie_dian_lcof
+    {
+        public ListNode DeleteNode(ListNode head, int val)
+        {
+            ListNode dumHead = new ListNode(0), pre = dumHead, cur = head;
+            dumHead.next = head;
+            while (cur != null)
+            {
+                if (cur.val == val)
+                    pre.next = cur.next;
+                cur = cur.next;
+                pre = pre.next;
+            }
+            return dumHead.next;
+        }
+    }
+}
