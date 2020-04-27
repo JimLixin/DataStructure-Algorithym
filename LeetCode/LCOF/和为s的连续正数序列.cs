87f9d77e7f64b6df5fb06d526bec9d1faa9ae293
Jim Li <lij42@airproducts.com>
2020-04-27 08:53:33 +0800

    面试题57 - II. 和为s的连续正数序列 面试题07. 重建二叉树

---
+++ LeetCode/LCOF/和为s的连续正数序列.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Algorithym.LeetCode.LCOF
+{
+    /// <summary>
+    /// 面试题57 - II. 和为s的连续正数序列
+    /// https://leetcode-cn.com/problems/he-wei-sde-lian-xu-zheng-shu-xu-lie-lcof/
+    /// </summary>
+    public class he_wei_sde_lian_xu_zheng_shu_xu_lie_lcof
+    {
+        public int[][] FindContinuousSequence(int target)
+        {
+            IList<int[]> result = new List<int[]>();
+            IList<int> data = new List<int>();
+            int index = 1, cur = index, sum = 0;
+            while (index < target / 2 + 1)
+            {
+                sum = 0;
+                cur = index;
+                data.Clear();
+                while (sum < target)
+                {
+                    sum += cur;
+                    data.Add(cur);
+                    cur++;
+                }
+                if (sum == target)
+                    result.Add(data.ToArray());
+                index++;
+            }
+            return result.ToArray();
+        }
+
+        /// <summary>
+        /// Better solution to use array
+        /// </summary>
+        /// <param name="target"></param>
+        /// <returns></returns>
+        public int[][] FindContinuousSequenceV2(int target)
+        {
+            IList<int[]> result = new List<int[]>();
+            int index = 1, cur = index, sum = 0;
+            while (index < target / 2 + 1)
+            {
+                sum = 0;
+                cur = index;
+                while (sum < target)
+                {
+                    sum += cur;
+                    cur++;
+                }
+                if (sum == target)
+                {
+                    result.Add(Enumerable.Range(index, cur - index).ToArray());
+                }
+                index++;
+            }
+            return result.ToArray();
+        }
+    }
+}
---
+++ LeetCode/LCOF/重建二叉树.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Algorithym.LeetCode.LCOF
+{
+    /// <summary>
+    /// 面试题07. 重建二叉树
+    /// https://leetcode-cn.com/problems/zhong-jian-er-cha-shu-lcof/
+    /// </summary>
+    public class zhong_jian_er_cha_shu_lcof
+    {
+        int index;
+        Dictionary<int, int> map = new Dictionary<int, int>();
+        public TreeNode BuildTree(int[] preorder, int[] inorder)
+        {
+            if (preorder == null || preorder.Length == 0)
+                return null;
+            for (int i = 0; i < inorder.Length; i++)
+            {
+                map.Add(inorder[i], i);
+            }
+            return build(preorder, inorder, 0, preorder.Length - 1);
+        }
+
+        public TreeNode build(int[] preorder, int[] inorder, int start, int end)
+        {
+            if (start > end)
+                return null;
+
+            int cur = preorder[index];
+            int curIndex = map[cur];
+            TreeNode node = new TreeNode(cur);
+            index++;
+
+            if (start == end)
+                return node;
+
+            node.left = build(preorder, inorder, start, curIndex - 1);
+            node.right = build(preorder, inorder, curIndex + 1, end);
+            return node;
+        }
+    }
+}
