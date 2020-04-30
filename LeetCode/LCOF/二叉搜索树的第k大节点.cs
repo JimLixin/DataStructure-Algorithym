24e4d634a63f84717d47ff5482d865d453e382a6
Jim Li <lij42@airproducts.com>
2020-04-30 09:22:02 +0800

    面试题54. 二叉搜索树的第k大节点

---
+++ LeetCode/LCOF/二叉搜索树的第k大节点.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Algorithym.LeetCode.LCOF
+{
+    /// <summary>
+    /// 面试题54. 二叉搜索树的第k大节点
+    /// https://leetcode-cn.com/problems/er-cha-sou-suo-shu-de-di-kda-jie-dian-lcof/
+    /// </summary>
+    public class 二叉搜索树的第k大节点
+    {
+        int count, result;
+        public int KthLargest(TreeNode root, int k)
+        {
+            travel(root, k);
+            return result;
+        }
+
+        public void travel(TreeNode node, int k)
+        {
+            if (node == null) return;
+            travel(node.right, k);
+            count++;
+            if (count == k)
+                result = node.val;
+            travel(node.left, k);
+        }
+    }
+}
--- LeetCode/LCOF/队列的最大值.cs
+++ LeetCode/LCOF/队列的最大值.cs
@@ -8,6 +8,10 @@ namespace Algorithym.LeetCode.LCOF
 {
     /// <summary>
     /// 面试题59 - II. 队列的最大值
+    /// TBD:解题思路
+    /// 为了解决上述问题，我们只需记住当前最大值出队后，队列里的下一个最大值即可。
+    /// 具体方法是使用一个双端队列 dequedeque，在每次入队时，如果 dequedeque 队尾元素小于即将入队的元素 valuevalue，
+    /// 则将小于 valuevalue 的元素全部出队后，再将 valuevalue 入队；否则直接入队。
     /// https://leetcode-cn.com/problems/dui-lie-de-zui-da-zhi-lcof/
     /// </summary>
     public class MaxQueue
