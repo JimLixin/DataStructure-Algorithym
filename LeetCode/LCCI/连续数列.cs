0a239caa3e68671ebe45f06f57f23af49fc9d76c
Jim Li <lij42@airproducts.com>
2020-05-01 19:21:07 +0800

    面试题 16.17. 连续数列

---
+++ LeetCode/LCCI/连续数列.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Algorithym.LeetCode.LCCI
+{
+    /// <summary>
+    /// 面试题 16.17. 连续数列
+    /// https://leetcode-cn.com/problems/contiguous-sequence-lcci/
+    /// </summary>
+    public class 连续数列
+    {
+        public int MaxSubArray(int[] nums)
+        {
+            int cur = nums[0], sum = cur;
+            for (int i = 1; i < nums.Length; i++)
+            {
+                cur = cur > 0 ? cur + nums[i] : nums[i];
+                if (cur > sum)
+                    sum = cur;
+            }
+            return sum;
+        }
+    }
+}
