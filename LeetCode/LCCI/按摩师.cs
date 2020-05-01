6343ca9af3c823b4b078d7070bd3a5c19d5ff754
Jim Li <lij42@airproducts.com>
2020-05-01 14:42:34 +0800

    面试题 17.16. 按摩师

---
+++ LeetCode/LCCI/按摩师.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Algorithym.LeetCode.LCCI
+{
+    /// <summary>
+    /// 面试题 17.16. 按摩师
+    /// https://leetcode-cn.com/problems/the-masseuse-lcci/
+    /// </summary>
+    public class 按摩师
+    {
+        public int Massage(int[] nums)
+        {
+            if (nums == null || nums.Length == 0)
+                return 0;
+            int len = nums.Length;
+            if (len == 1) return nums[0];
+            if (len < 3) return Math.Max(nums[0], nums[1]);
+            int[] dp = new int[3];
+            dp[0] = nums[0];
+            dp[1] = Math.Max(nums[0], nums[1]);
+
+            for (int i = 2; i < len; i++)
+            {
+                dp[i % 3] = Math.Max(nums[i] + dp[(i - 2) % 3], dp[(i - 1) % 3]);
+            }
+            return dp[(len - 1) % 3];
+        }
+    }
+}
---
+++ LeetCode/LCOF/二叉搜索树与双向链表.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Algorithym.LeetCode.LCOF
+{
+    public class Node
+    {
+        public int val;
+        public Node left;
+        public Node right;
+
+        public Node() { }
+        public Node(int _val, Node _left, Node _right)
+        {
+            val = _val;
+            left = _left;
+            right = _right;
+        }
+
+    }
+    public class 二叉搜索树与双向链表
+    {
+        Node head, pre;
+        public Node TreeToDoublyList(Node root)
+        {
+            travel(root);
+            head.left = pre;
+            pre.right = head;
+            return head;
+        }
+
+        public void travel(Node node)
+        {
+            if (node == null) return;
+            travel(node.left);
+            if (head == null)
+            {
+                head = node;
+                pre = node;
+            }
+            else
+            {
+                pre.right = node;
+                node.left = pre;
+                pre = node;
+            }
+            travel(node.right);
+        }
+    }
+}
