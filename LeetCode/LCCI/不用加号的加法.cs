dfa82801ccb25edf48e57ac6ccf5f4d45c05fc4d
Jim Li <lij42@airproducts.com>
2020-05-01 16:05:14 +0800

    面试题 17.01. 不用加号的加法

---
+++ LeetCode/LCCI/不用加号的加法.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Algorithym.LeetCode.LCCI
+{
+    /// <summary>
+    /// 面试题 17.01. 不用加号的加法
+    /// https://leetcode-cn.com/problems/add-without-plus-lcci/
+    /// </summary>
+    public class 不用加号的加法
+    {
+        public int Add(int a, int b)
+        {
+            int tmp;
+            while (b != 0)
+            {
+                tmp = a ^ b;
+                b = (a & b) << 1;
+                a = tmp;
+            }
+            return a;
+        }
+    }
+}
