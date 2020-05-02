d939d4ebb125517358f154c9cc2b1c7d2750198a
Jim Li <lij42@airproducts.com>
2020-05-02 20:45:40 +0800

    面试题 10.01. 合并排序的数组

---
+++ LeetCode/LCCI/合并排序的数组.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Algorithym.LeetCode.LCCI
+{
+    /// <summary>
+    /// 面试题 10.01. 合并排序的数组
+    /// https://leetcode-cn.com/problems/sorted-merge-lcci/
+    /// </summary>
+    public class sorted_merge_lcci
+    {
+        public void Merge(int[] A, int m, int[] B, int n)
+        {
+            int end = (m + n - 1);
+            while (n > 0 && m > 0)
+            {
+                if (A[m - 1] > B[n - 1])
+                {
+                    A[end] = A[m - 1];
+                    m--;
+                }
+                else
+                {
+                    A[end] = B[n - 1];
+                    n--;
+                }
+                end--;
+            }
+            while (n > 0)
+            {
+                A[end] = B[n - 1];
+                n--;
+                end--;
+            }
+            while (m > 0)
+            {
+                A[end] = A[m - 1];
+                m--;
+                end--;
+            }
+        }
+    }
+}
--- LeetCode/Topics/双指针.cs
+++ LeetCode/Topics/双指针.cs
@@ -323,4 +323,42 @@ namespace Algorithym.LeetCode.Topics
             return slow.val;
         }
     }
+
+    /// <summary>
+    /// 面试题 10.01. 合并排序的数组
+    /// https://leetcode-cn.com/problems/sorted-merge-lcci/
+    /// </summary>
+    public class sorted_merge_lcci
+    {
+        public void Merge(int[] A, int m, int[] B, int n)
+        {
+            int end = (m + n - 1);
+            while (n > 0 && m > 0)
+            {
+                if (A[m - 1] > B[n - 1])
+                {
+                    A[end] = A[m - 1];
+                    m--;
+                }
+                else
+                {
+                    A[end] = B[n - 1];
+                    n--;
+                }
+                end--;
+            }
+            while (n > 0)
+            {
+                A[end] = B[n - 1];
+                n--;
+                end--;
+            }
+            while (m > 0)
+            {
+                A[end] = A[m - 1];
+                m--;
+                end--;
+            }
+        }
+    }
 }
