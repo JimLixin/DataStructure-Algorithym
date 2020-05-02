b83e8236920e83eab2c60b495ba0b500b5b6e1c3
Jim Li <lij42@airproducts.com>
2020-05-02 13:12:52 +0800

    234. 回文链表

---
+++ LeetCode/00234_palindrome-linked-list.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Algorithym.LeetCode
+{
+    /// <summary>
+    /// 234. 回文链表
+    /// https://leetcode-cn.com/problems/palindrome-linked-list/
+    /// </summary>
+    public class palindrome_linked_list
+    {
+        public bool IsPalindrome(ListNode head)
+        {
+            if (head == null) return true;
+            ListNode slow = head, fast = head;
+            Stack<int> s = new Stack<int>();
+            while (true)
+            {
+                if (fast == null) break;
+                if (fast.next == null)
+                {
+                    slow = slow.next;
+                    break;
+                }
+                s.Push(slow.val);
+                slow = slow.next;
+                fast = fast.next.next;
+            }
+            while (s.Count > 0)
+            {
+                if (s.Pop() != slow.val)
+                    return false;
+                slow = slow.next;
+            }
+            return true;
+        }
+    }
+}
--- LeetCode/Topics/双指针.cs
+++ LeetCode/Topics/双指针.cs
@@ -129,4 +129,37 @@ namespace Algorithym.LeetCode.Topics
             return new int[0];
         }
     }
+
+    /// <summary>
+    /// 234. 回文链表
+    /// https://leetcode-cn.com/problems/palindrome-linked-list/
+    /// </summary>
+    public class palindrome_linked_list
+    {
+        public bool IsPalindrome(ListNode head)
+        {
+            if (head == null) return true;
+            ListNode slow = head, fast = head;
+            Stack<int> s = new Stack<int>();
+            while (true)
+            {
+                if (fast == null) break;
+                if (fast.next == null)
+                {
+                    slow = slow.next;
+                    break;
+                }
+                s.Push(slow.val);
+                slow = slow.next;
+                fast = fast.next.next;
+            }
+            while (s.Count > 0)
+            {
+                if (s.Pop() != slow.val)
+                    return false;
+                slow = slow.next;
+            }
+            return true;
+        }
+    }
 }
