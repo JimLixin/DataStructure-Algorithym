48d5abf7ad4b800e11402aec626aba424c48f3e9
Jim Li <lij42@airproducts.com>
2020-05-02 13:24:29 +0800

    344. 反转字符串

---
+++ LeetCode/00344_reverse-string.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Algorithym.LeetCode
+{
+    /// <summary>
+    /// 344. 反转字符串
+    /// https://leetcode-cn.com/problems/reverse-string/
+    /// </summary>
+    public class reverse_string
+    {
+        public void ReverseString(char[] s)
+        {
+            if (s == null || s.Length == 0) return;
+            int left = 0, right = s.Length - 1;
+            char tmp;
+            while (left < right)
+            {
+                tmp = s[right];
+                s[right] = s[left];
+                s[left] = tmp;
+                left++;
+                right--;
+            }
+        }
+    }
+}
--- LeetCode/Topics/双指针.cs
+++ LeetCode/Topics/双指针.cs
@@ -162,4 +162,26 @@ namespace Algorithym.LeetCode.Topics
             return true;
         }
     }
+
+    /// <summary>
+    /// 344. 反转字符串
+    /// https://leetcode-cn.com/problems/reverse-string/
+    /// </summary>
+    public class reverse_string
+    {
+        public void ReverseString(char[] s)
+        {
+            if (s == null || s.Length == 0) return;
+            int left = 0, right = s.Length - 1;
+            char tmp;
+            while (left < right)
+            {
+                tmp = s[right];
+                s[right] = s[left];
+                s[left] = tmp;
+                left++;
+                right--;
+            }
+        }
+    }
 }
