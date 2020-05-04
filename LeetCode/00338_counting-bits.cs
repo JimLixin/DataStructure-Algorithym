92afc16ea5a6ffb613cb5b1be30ace30b7c7834e
Jim Li <lij42@airproducts.com>
2020-05-04 13:24:15 +0800

    338. 比特位计数

---
+++ LeetCode/00338_counting-bits.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Algorithym.LeetCode
+{
+    /// <summary>
+    /// 338. 比特位计数
+    /// https://leetcode-cn.com/problems/counting-bits/
+    /// </summary>
+    public class _00338_counting_bits
+    {
+        public int[] CountBits(int num)
+        {
+            if (num < 0) return new int[0];
+            int len = num + 1, count = 1, start = 0;
+            int[] dp = new int[len];
+            dp[0] = 0;
+            for (int i = 1; i < len; i++)
+            {
+                if ((i & 1) > 0)
+                    dp[i] = dp[i - 1] + 1;
+                else if ((i & (i - 1)) == 0)
+                {
+                    dp[i] = 1;
+                    count = 1;
+                    start = i;
+                }
+                else
+                    dp[i] = count + dp[i - start];
+            }
+            return dp;
+        }
+    }
+}
---
+++ LeetCode/LCOF/042_连续子数组的最大和.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Algorithym.LeetCode.LCOF
+{
+    /// <summary>
+    /// 面试题42. 连续子数组的最大和
+    /// https://leetcode-cn.com/problems/lian-xu-zi-shu-zu-de-zui-da-he-lcof/
+    /// </summary>
+    public class _042_连续子数组的最大和
+    {
+        public int MaxSubArray(int[] nums)
+        {
+            if (nums == null || nums.Length == 0)
+                return 0;
+            int cur = nums[0], max = cur;
+            for (int i = 1; i < nums.Length; i++)
+            {
+                cur = cur > 0 ? cur + nums[i] : nums[i];
+                max = Math.Max(max, cur);
+            }
+            return max;
+        }
+    }
+}
