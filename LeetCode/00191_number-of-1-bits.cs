02490e22793ec76ca3f1fb6925c9e6f0a9b7e59c
Jim Li <lij42@airproducts.com>
2020-05-04 13:30:49 +0800

    191. 位1的个数

---
+++ LeetCode/00191_number-of-1-bits.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Algorithym.LeetCode
+{
+    /// <summary>
+    /// 191. 位1的个数
+    /// https://leetcode-cn.com/problems/number-of-1-bits/
+    /// </summary>
+    public class _00191_number_of_1_bits
+    {
+        public int HammingWeight(uint n)
+        {
+            int count = 0;
+            while (n > 0)
+            {
+                count += (n & 1) > 0 ? 1 : 0;
+                n >>= 1;
+            }
+            return count;
+        }
+    }
+}
