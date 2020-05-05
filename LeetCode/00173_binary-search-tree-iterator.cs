770ee74b7e191b2da3b31a4ba3f3fddc545f108a
Jim Li <lij42@airproducts.com>
2020-05-05 13:57:09 +0800

    173. 二叉搜索树迭代器 230. 二叉搜索树中第K小的元素

---
+++ LeetCode/00173_binary-search-tree-iterator.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+/// <summary>
+/// 173. 二叉搜索树迭代器
+/// https://leetcode-cn.com/problems/binary-search-tree-iterator/
+/// </summary>
+namespace Algorithym.LeetCode
+{
+    /// <summary>
+    /// Space: O(N)
+    /// </summary>
+    public class BSTIterator
+    {
+        private Queue<int> data = null;
+        public BSTIterator(TreeNode root)
+        {
+            data = new Queue<int>();
+            travel(root);
+        }
+
+        private void travel(TreeNode node)
+        {
+            if (node == null)
+                return;
+            travel(node.left);
+            data.Enqueue(node.val);
+            travel(node.right);
+        }
+
+        /** @return the next smallest number */
+        public int Next()
+        {
+            return data.Dequeue();
+        }
+
+        /** @return whether we have a next smallest number */
+        public bool HasNext()
+        {
+            return data.Count > 0;
+        }
+    }
+
+    /// <summary>
+    /// Space: O(h)
+    /// Note: h是BST的高度
+    /// </summary>
+    public class BSTIteratorV2
+    {
+        private Stack<TreeNode> s = null;
+        public BSTIteratorV2(TreeNode root)
+        {
+            s = new Stack<TreeNode>();
+            travel(root);
+        }
+
+        private void travel(TreeNode node)
+        {
+            while (node != null)
+            {
+                s.Push(node);
+                node = node.left;
+            }
+        }
+
+        /** @return the next smallest number */
+        public int Next()
+        {
+            TreeNode node = s.Pop();
+            if (node.right != null)
+            {
+                travel(node.right);
+            }
+            return node.val;
+        }
+
+        /** @return whether we have a next smallest number */
+        public bool HasNext()
+        {
+            return s.Count > 0;
+        }
+    }
+}
---
+++ LeetCode/00230_kth-smallest-element-in-a-bst.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Algorithym.LeetCode
+{
+    /// <summary>
+    /// 230. 二叉搜索树中第K小的元素
+    /// https://leetcode-cn.com/problems/kth-smallest-element-in-a-bst/
+    /// </summary>
+    public class _00230_kth_smallest_element_in_a_bst
+    {
+        public int KthSmallest(TreeNode root, int k)
+        {
+            Stack<TreeNode> s = new Stack<TreeNode>();
+            int count = 0;
+            TreeNode node = root;
+            while (node != null || s.Count > 0)
+            {
+                while (node != null)
+                {
+                    s.Push(node);
+                    node = node.left;
+                }
+                node = s.Pop();
+                count++;
+                if (count == k)
+                    return node.val;
+                node = node.right;
+            }
+            return node.val;
+        }
+    }
+}
