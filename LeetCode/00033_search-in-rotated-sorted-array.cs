716914440a055ee88b88a9f500a0f2166ee99fb8
Jim Li <lij42@airproducts.com>
2020-05-07 21:18:27 +0800

    33. 搜索旋转排序数组

---
+++ LeetCode/00033_search-in-rotated-sorted-array.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+/// <summary>
+/// 33. Search in Rotated Sorted Array
+/// https://leetcode-cn.com/problems/search-in-rotated-sorted-array/
+/// </summary>
+namespace Algorithym.LeetCode
+{
+    public class _00033_search_in_rotated_sorted_array
+    {
+        public int Search(int[] nums, int target)
+        {
+            if (nums == null || nums.Length == 0)
+                return -1;
+            int mid = -1, start = 0, end = nums.Length - 1;
+            while (end - start > 1)
+            {
+                mid = (start + end) / 2;
+                if (nums[mid] > nums[start])
+                    start = mid;
+                else
+                    end = mid;
+            }
+            int index1 = binarySearch(nums, 0, end - 1, target);
+            int index2 = binarySearch(nums, end, nums.Length - 1, target);
+            if (index1 < 0 && index2 < 0) return -1;
+            return index1 < 0 ? index2 : index1;
+        }
+
+        public int binarySearch(int[] nums, int start, int end, int target)
+        {
+            int mid;
+            while (start <= end)
+            {
+                mid = (start + end) / 2;
+                if (nums[mid] == target)
+                    return mid;
+                if (nums[mid] > target)
+                    end = mid - 1;
+                else
+                    start = mid + 1;
+            }
+            return -1;
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -13,8 +13,30 @@ namespace Algorithym
 {
     class Program
     {
+        public static int binarySearch(int[] nums, int start, int end, int target)
+        {
+            
+            while (start <= end)
+            {
+                int mid = (start + end) / 2;
+                if (nums[mid] == target)
+                    return mid;
+                if (nums[mid] > target)
+                    end = mid - 1;
+                else
+                    start = mid + 1;
+            }
+            return -1;
+        }
+
         static void Main(string[] args)
         {
+            
+            int[] nums2 = new int[] { 0, 1, 2 ,4, 5, 6, 7,9};
+            var binRes = binarySearch(nums2, 0 , nums2.Length - 1, 0);
+
+            List<int> list = new List<int>(new int[] { 1,3,5,7,9});
+            var biRes = list.BinarySearch(4);
             int[] nums = new int[] {3,0,1};
             (new missing_number()).MissingNumber(nums);
 
