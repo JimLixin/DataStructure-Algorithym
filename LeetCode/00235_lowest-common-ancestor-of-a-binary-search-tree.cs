1ac76a4c9d15fa13a52f92e9e963da23a8755522
Jim Li <lij42@airproducts.com>
2020-05-10 15:16:39 +0800

    235. 二叉搜索树的最近公共祖先 236. 二叉树的最近公共祖先

---
+++ LeetCode/00235_lowest-common-ancestor-of-a-binary-search-tree.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+/// <summary>
+/// 235. 二叉搜索树的最近公共祖先
+/// https://leetcode-cn.com/problems/lowest-common-ancestor-of-a-binary-search-tree/
+/// </summary>
+namespace Algorithym.LeetCode
+{
+    public class _00235_lowest_common_ancestor_of_a_binary_search_tree
+    {
+        public TreeNode LowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q)
+        {
+            if (root == null) return null;
+            if (root.val == p.val || root.val == q.val)
+                return root;
+            if (p.val > root.val && q.val > root.val)
+                return LowestCommonAncestor(root.right, p, q);
+            if (p.val < root.val && q.val < root.val)
+                return LowestCommonAncestor(root.left, p, q);
+            return root;
+        }
+    }
+}
---
+++ LeetCode/00236_lowest-common-ancestor-of-a-binary-tree.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+/// <summary>
+/// 236. 二叉树的最近公共祖先
+/// https://leetcode-cn.com/problems/lowest-common-ancestor-of-a-binary-tree/
+/// </summary>
+namespace Algorithym.LeetCode
+{
+    public class _00236_lowest_common_ancestor_of_a_binary_tree
+    {
+        public TreeNode LowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q)
+        {
+            if (root == null) return null;
+            if (root == q || root == p) return root;
+            TreeNode left = LowestCommonAncestor(root.left, p, q);
+            TreeNode right = LowestCommonAncestor(root.right, p, q);
+            if (left != null && right != null)
+                return root;
+            return left != null ? left : right;
+        }
+
+    }
+}
