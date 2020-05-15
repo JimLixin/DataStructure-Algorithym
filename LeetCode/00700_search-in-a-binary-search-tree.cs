fb0d85fc5a8e4d4f3f5e5b9a5a727dc1db262850
Jim Li <lij42@airproducts.com>
2020-05-15 20:36:28 +0800

    700. 二叉搜索树中的搜索

---
+++ LeetCode/00700_search-in-a-binary-search-tree.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+/// <summary>
+/// 700. 二叉搜索树中的搜索
+/// https://leetcode-cn.com/problems/search-in-a-binary-search-tree/
+/// </summary>
+namespace Algorithym.LeetCode
+{
+    public class _00700_search_in_a_binary_search_tree
+    {
+        public TreeNode SearchBST(TreeNode root, int val)
+        {
+            if (root == null) return null;
+            if (root.val == val) return root;
+            return root.val > val ? SearchBST(root.left, val) : SearchBST(root.right, val);
+        }
+    }
+}
