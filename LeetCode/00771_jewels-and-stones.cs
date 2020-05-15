c966efd0f5f917e72906bf47752c80133d8f6c41
Jim Li <lij42@airproducts.com>
2020-05-15 19:50:34 +0800

    771. 宝石与石头

---
+++ LeetCode/00771_jewels-and-stones.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+/// <summary>
+/// 771. 宝石与石头
+/// https://leetcode-cn.com/problems/jewels-and-stones/
+/// </summary>
+namespace Algorithym.LeetCode
+{
+    public class _00771_jewels_and_stones
+    {
+        public int NumJewelsInStones(string J, string S)
+        {
+            if (string.IsNullOrEmpty(J) || string.IsNullOrEmpty(S))
+                return 0;
+            int count = 0;
+            HashSet<char> map = new HashSet<char>(J.ToCharArray());
+            foreach (char c in S)
+            {
+                if (map.Contains(c))
+                    count++;
+            }
+            return count;
+        }
+    }
+}
