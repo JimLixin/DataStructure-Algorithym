998bf7400c34107ea2dfc4e68eb381dfd62662fb
Jim Li <lij42@airproducts.com>
2020-05-15 19:35:34 +0800

    面试题 02.03. 删除中间节点

---
+++ LeetCode/LCCI/删除中间节点.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+/// <summary>
+/// 面试题 02.03. 删除中间节点
+/// https://leetcode-cn.com/problems/delete-middle-node-lcci/
+/// </summary>
+namespace Algorithym.LeetCode.LCCI
+{
+    public class Solution
+    {
+        public void DeleteNode(ListNode node)
+        {
+            node.val = node.next.val;
+            node.next = node.next.next;
+        }
+    }
+}
