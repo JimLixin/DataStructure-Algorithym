dd3ae46b6105e66e97e985c7d8cacfbff5c8cf1a
Jim Li <lij42@airproducts.com>
2020-05-16 10:49:03 +0800

    LCP 01. 猜数字

---
+++ LeetCode/LCP/01_猜数字.cpp
@@ -0,0 +1,14 @@
+﻿//LCP 01. 猜数字
+//https://leetcode-cn.com/problems/guess-numbers/
+class Solution {
+public:
+    int game(vector<int>& guess, vector<int>& answer) {
+        int count = 0;
+        for (int i = 0; i < 3; i++)
+        {
+            count += (guess[i] == answer[i] ? 1 : 0);
+        }
+        //std::cout << "hello world!" << guess.size() << std::endl;
+        return count;
+    }
+};
