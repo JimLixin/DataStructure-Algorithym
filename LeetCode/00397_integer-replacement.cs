42c8cbda7f7e9a6beee81e0dace3e5f8067196de
Jim Li <lij42@airproducts.com>
2020-05-16 18:45:42 +0800

    397. 整数替换

---
+++ LeetCode/00397_integer-replacement.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+/// <summary>
+/// 397. 整数替换
+/// https://leetcode-cn.com/problems/integer-replacement/
+/// </summary>
+namespace Algorithym.LeetCode
+{
+    public class _00397_integer_replacement
+    {
+        public int IntegerReplacement(int n)
+        {
+            long N = n;
+            int steps = 0;
+            while (N > 1)
+            {
+                if ((N & 1) == 0)
+                    N >>= 1;
+                else
+                    N += ((N & 2) == 0 || N == 3) ? -1 : 1;
+                steps++;
+            }
+            return steps;
+        }
+    }
+}
