1970f325478ecc511f837d7681a12af267984de7
Jim Li <lij42@airproducts.com>
2020-05-16 15:25:06 +0800

    326. 3的幂

---
+++ LeetCode/00326_power-of-threeClass1.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+/// <summary>
+/// 326. 3的幂
+/// https://leetcode-cn.com/problems/power-of-three/
+/// </summary>
+namespace Algorithym.LeetCode
+{
+    public class _00326_power_of_three
+    {
+        /// <summary>
+        /// 递归版本
+        /// </summary>
+        /// <param name="n"></param>
+        /// <returns></returns>
+        public bool IsPowerOfThree(int n)
+        {
+            if (n <= 0) return false;
+            if (n == 1) return true;
+            return n % 3 == 0 ? IsPowerOfThree(n / 3) : false;
+        }
+
+        /// <summary>
+        /// 迭代版本
+        /// </summary>
+        /// <param name="n"></param>
+        /// <returns></returns>
+        public bool IsPowerOfThreeV2(int n)
+        {
+            if (n <= 0) return false;
+            while (n % 3 == 0)
+            {
+                n /= 3;
+            }
+            return n == 1;
+        }
+    }
+}
---
+++ LeetCode/位运算/2的幂.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+/// <summary>
+/// 231. 2的幂
+/// https://leetcode-cn.com/problems/power-of-two/
+/// </summary>
+namespace Algorithym.LeetCode.位运算
+{
+    public class _00231_power_of_two
+    {
+        public bool IsPowerOfTwo(int n)
+        {
+            return n <= 0 ? false : (n & (n - 1)) == 0;
+        }
+    }
+}
