c85b987ac77c979ac2063a6b65689f7eef318ecd
Jim Li <lij42@airproducts.com>
2020-05-16 11:07:39 +0800

    1342. 将数字变成 0 的操作次数

---
+++ LeetCode/01342_number-of-steps-to-reduce-a-number-to-zero.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+/// <summary>
+/// 1342. 将数字变成 0 的操作次数
+/// https://leetcode-cn.com/problems/number-of-steps-to-reduce-a-number-to-zero/
+/// </summary>
+namespace Algorithym.LeetCode
+{
+    public class _01342_number_of_steps_to_reduce_a_number_to_zero
+    {
+        public int NumberOfSteps(int num)
+        {
+            int steps = 0;
+            while (num != 0)
+            {
+                steps += (num & 1) == 0 ? 1 : 2;
+                num >>= 1;
+            }
+            return --steps;
+        }
+    }
+}
