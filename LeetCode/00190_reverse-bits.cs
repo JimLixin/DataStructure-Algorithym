5785b9a6e34842050a24a5ef25adb21095d18e2c
Jim Li <lij42@airproducts.com>
2020-05-16 11:27:58 +0800

    190. 颠倒二进制位

---
+++ LeetCode/00190_reverse-bits.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+/// <summary>
+/// 190. 颠倒二进制位
+/// https://leetcode-cn.com/problems/reverse-bits/
+/// </summary>
+namespace Algorithym.LeetCode
+{
+    public class _00190_reverse_bits
+    {
+        public uint reverseBits(uint n)
+        {
+            uint result = 0;
+            for (int i = 0; i < 32; i++)
+            {
+                result <<= 1;
+                result |= (n & 1);
+                n >>= 1;
+            }
+            return result;
+        }
+    }
+}
