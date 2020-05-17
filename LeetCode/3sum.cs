afe3b1ca4e9e2cca673059b73f686ee7979c1de9
Jim Li <lij42@airproducts.com>
2020-05-17 10:12:22 +0800

    15. 三数之和

---
+++ LeetCode/3sum.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+/// <summary>
+/// 15. 三数之和
+/// https://leetcode-cn.com/problems/3sum/
+/// </summary>
+namespace Algorithym.LeetCode
+{
+    public class _3sum
+    {
+        public IList<IList<int>> ThreeSum(int[] nums)
+        {
+            IList<IList<int>> result = new List<IList<int>>();
+            if (nums == null || nums.Length == 0) return result;
+            Array.Sort(nums);
+            for (int i = 0; i < nums.Length - 1; i++)
+            {
+                if (nums[i] > 0) break;
+                if (i > 0 && nums[i] == nums[i - 1]) continue;
+                int left = i + 1, right = nums.Length - 1;
+                while (left < right)
+                {
+                    int sum = nums[i] + nums[left] + nums[right];
+                    if (sum == 0)
+                    {
+                        result.Add(new List<int>(new int[] { nums[i], nums[left], nums[right] }));
+                        while (left < right && nums[left] == nums[left + 1]) left++;
+                        while (left < right && nums[right] == nums[right - 1]) right--;
+                        left++;
+                        right--;
+                    }
+                    else if (sum < 0)
+                        left++;
+                    else
+                        right--;
+                }
+            }
+            return result;
+        }
+    }
+}
