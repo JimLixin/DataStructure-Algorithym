1286bfb3c6e91852a243a628b1fe3db0b94a40f9
Jim Li <lij42@airproducts.com>
2020-05-22 20:47:28 +0800

    139. 单词拆分

---
+++ LeetCode/00139_word-break.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+/// <summary>
+/// 139. 单词拆分
+/// https://leetcode-cn.com/problems/word-break/
+/// </summary>
+namespace Algorithym.LeetCode
+{
+    public class _00139_word_break
+    {
+        public bool WordBreak(string s, IList<string> wordDict)
+        {
+            if (string.IsNullOrEmpty(s)) return false;
+            HashSet<string> map = new HashSet<string>(wordDict);
+            bool[] dp = new bool[s.Length + 1];
+            dp[0] = true;
+            for (int i = 1; i <= s.Length; i++)
+            {
+                for (int j = 0; j < i; j++)
+                {
+                    if (dp[j] && map.Contains(s.Substring(j, i - j)))
+                    {
+                        dp[i] = true;
+                        break;
+                    }
+                }
+            }
+            return dp[s.Length];
+        }
+    }
+}
