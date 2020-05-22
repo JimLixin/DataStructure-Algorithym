bf188b60854da2abd65373302fd8f6ae19092394
Jim Li <lij42@airproducts.com>
2020-05-22 15:43:38 +0800

    48. 旋转图像 566. 重塑矩阵

---
+++ LeetCode/00048_rotate-image.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+/// <summary>
+/// 48. 旋转图像
+/// https://leetcode-cn.com/problems/rotate-image/
+/// </summary>
+namespace Algorithym.LeetCode
+{
+    public class _00048_rotate_image
+    {
+        public void Rotate(int[][] matrix)
+        {
+            if (matrix == null || matrix.Length == 0 || matrix[0].Length == 0)
+                return;
+            int n = matrix.Length;
+            for (int i = 0; i < n; i++)
+            {
+                int left = 0, right = n - 1;
+                while (left < right)
+                {
+                    matrix[i][left] = matrix[i][left] ^ matrix[i][right];
+                    matrix[i][right] = matrix[i][left] ^ matrix[i][right];
+                    matrix[i][left] = matrix[i][left] ^ matrix[i][right];
+                    left++;
+                    right--;
+                }
+            }
+            int k = n;
+            for (int i = 0; i < n; i++)
+            {
+                for (int j = 0; j < k; j++)
+                {
+                    int tmp;
+                    tmp = matrix[i][j];
+                    matrix[i][j] = matrix[n - j - 1][n - i - 1];
+                    matrix[n - j - 1][n - i - 1] = tmp;
+                }
+                k--;
+            }
+        }
+    }
+}
---
+++ LeetCode/00566_reshape-the-matrix.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+/// <summary>
+/// 566. 重塑矩阵
+/// https://leetcode-cn.com/problems/reshape-the-matrix/
+/// </summary>
+namespace Algorithym.LeetCode
+{
+    public class _00566_reshape_the_matrix
+    {
+        public int[][] MatrixReshape(int[][] nums, int r, int c)
+        {
+            if (nums == null || nums.Length == 0 || nums[0].Length == 0) return nums;
+            int rows = nums.Length, cols = nums[0].Length;
+            if (r * c != rows * cols) return nums;
+            int[][] data = new int[r][];
+            for (int i = 0; i < r; i++)
+            {
+                data[i] = new int[c];
+                for (int j = 0; j < c; j++)
+                {
+                    int cur = c * i + j + 1;
+                    data[i][j] = nums[(cur - 1) / cols][(cur - 1) % cols];
+                }
+            }
+            return data;
+        }
+    }
+}
