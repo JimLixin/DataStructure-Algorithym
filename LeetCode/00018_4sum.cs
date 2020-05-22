65e987d7ec82f457fd95616a9d984c5c130f7204
Jim Li <lij42@airproducts.com>
2020-05-22 10:40:24 +0800

    18. 四数之和

---
+++ LeetCode/00018_4sum.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+/// <summary>
+/// 18. 四数之和
+/// https://leetcode-cn.com/problems/4sum/
+/// </summary>
+namespace Algorithym.LeetCode
+{
+    public class _00018_4sum
+    {
+        public IList<IList<int>> FourSum(int[] nums, int target)
+        {
+            IList<IList<int>> result = new List<IList<int>>();
+            if (nums == null || nums.Length == 0)
+                return result;
+            Array.Sort(nums);
+            int len = nums.Length;
+            for (int i = 0; i < len - 3; i++)
+            {
+                if (i > 0 && nums[i] == nums[i - 1]) continue;
+                for (int j = i + 1; j < len - 2; j++)
+                {
+                    if (j > i + 1 && nums[j] == nums[j - 1]) continue;
+                    int l = j + 1, r = len - 1;
+                    while (l < r)
+                    {
+                        int sum = nums[i] + nums[j] + nums[l] + nums[r];
+                        if (sum == target)
+                        {
+                            result.Add(new int[] { nums[i], nums[j], nums[l], nums[r] });
+                            while (l < r && nums[l + 1] == nums[l]) l++;
+                            while (l < r && nums[r - 1] == nums[r]) r--;
+                            r--;
+                            l++;
+                        }
+                        else if (sum > target)
+                            r--;
+                        else
+                            l++;
+                    }
+                }
+            }
+
+            return result;
+        }
+    }
+}
