57178158f46025b2af40246857cd0bfecb580ae5
Jim Li <lij42@airproducts.com>
2020-05-23 20:37:12 +0800

    面试题 04.03. 特定深度节点链表

---
+++ LeetCode/LCCI/特定深度节点链表.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+/// <summary>
+/// 面试题 04.03. 特定深度节点链表
+/// https://leetcode-cn.com/problems/list-of-depth-lcci/
+/// </summary>
+namespace Algorithym.LeetCode.LCCI
+{
+    public class 特定深度节点链表
+    {
+        public ListNode[] ListOfDepth(TreeNode tree)
+        {
+            IList<ListNode> result = new List<ListNode>();
+            if (tree == null) return result.ToArray();
+            int level = 0;
+            Queue<TreeNode> q = new Queue<TreeNode>();
+            q.Enqueue(tree);
+            while (q.Count > 0)
+            {
+                int cnt = q.Count;
+                ListNode head = new ListNode(), tail = null;
+                for (int i = 0; i < cnt; i++)
+                {
+                    TreeNode node = q.Dequeue();
+                    if (head.next == null)
+                    {
+                        head.next = new ListNode(node.val);
+                        tail = head.next;
+                    }
+                    else
+                    {
+                        tail.next = new ListNode(node.val);
+                        tail = tail.next;
+                    }
+                    if (node.left != null)
+                        q.Enqueue(node.left);
+                    if (node.right != null)
+                        q.Enqueue(node.right);
+                }
+                result.Add(head.next);
+            }
+            return result.ToArray();
+        }
+    }
+}
