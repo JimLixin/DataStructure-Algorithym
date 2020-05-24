24c580fc50f992a1200a08d97a36a17b8eef1bc5
Jim Li <lij42@airproducts.com>
2020-05-24 16:24:06 +0800

    面试题 04.05. 合法二叉搜索树

---
+++ LeetCode/LCCI/合法二叉搜索树.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+/// <summary>
+/// 面试题 04.05. 合法二叉搜索树
+/// https://leetcode-cn.com/problems/legal-binary-search-tree-lcci/
+/// </summary>
+namespace Algorithym.LeetCode.LCCI
+{
+    public class 合法二叉搜索树
+    {
+        public bool IsValidBST(TreeNode root)
+        {
+            if (root == null) return true;
+            int? max = null;
+            Stack<TreeNode> s = new Stack<TreeNode>();
+            TreeNode node = root;
+            while (node != null || s.Count > 0)
+            {
+                while (node != null)
+                {
+                    s.Push(node);
+                    node = node.left;
+                }
+                node = s.Pop();
+                if (max != null && node.val <= max)
+                    return false;
+                else
+                    max = node.val;
+                node = node.right;
+            }
+            return true;
+        }
+    }
+}
