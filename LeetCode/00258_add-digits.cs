7c5b674bb6b5e1f21d792296cf58dec7e02d8064
Jim Li <lij42@airproducts.com>
2020-05-25 20:20:11 +0800

    258. 各位相加

---
+++ LeetCode/00258_add-digits.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+/// <summary>
+/// 258. 各位相加
+/// https://leetcode-cn.com/problems/add-digits/
+/// </summary>
+namespace Algorithym.LeetCode
+{
+    public class _00258_add_digits
+    {
+        public int AddDigits(int num)
+        {
+            while (num >= 10)
+            {
+                int result = 0;
+                while (num != 0)
+                {
+                    result += num % 10;
+                    num /= 10;
+                }
+                num = result;
+            }
+            return num;
+        }
+    }
+}
