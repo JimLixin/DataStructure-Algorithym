c8b028c9d2bf4e566b778ee5c60b539c1c838a22
Jim Li <lij42@airproducts.com>
2020-05-29 10:28:28 +0800

    331. 验证二叉树的前序序列化

---
+++ LeetCode/00331_验证二叉树的前序序列化.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+/// <summary>
+/// 331. 验证二叉树的前序序列化
+/// https://leetcode-cn.com/problems/verify-preorder-serialization-of-a-binary-tree/
+/// </summary>
+namespace Algorithym.LeetCode
+{
+    public class _00331_验证二叉树的前序序列化
+    {
+        int index, len;
+        string[] nodes;
+        public bool IsValidSerialization(string preorder)
+        {
+            nodes = preorder.Split(",".ToCharArray());
+            index = 0;
+            len = nodes.Length;
+            return dfs() && index == len;
+        }
+
+        public bool dfs()
+        {
+            if (index >= len) return false;
+            return nodes[index++] == "#" ? true : dfs() && dfs();
+        }
+    }
+}
