c3b2271236b012186fdd4daaedd0f3713c5d81bf
Jim Li <lij42@airproducts.com>
2020-05-29 13:34:37 +0800

    455. 分发饼干

---
+++ LeetCode/00455_分发饼干.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+/// <summary>
+/// 455. 分发饼干
+/// https://leetcode-cn.com/problems/assign-cookies/
+/// </summary>
+namespace Algorithym.LeetCode
+{
+    public class _00455_分发饼干
+    {
+        public int FindContentChildren(int[] g, int[] s)
+        {
+            if (g == null || g.Length == 0 || s == null || s.Length == 0) return 0;
+            Array.Sort(g);
+            Array.Sort(s);
+            int index = 0, count = 0;
+            for (int i = 0; i < s.Length; i++)
+            {
+                if (index >= g.Length) break;
+                if (g[index] <= s[i])
+                {
+                    count++;
+                    index++;
+                }
+            }
+            return count;
+        }
+    }
+}
