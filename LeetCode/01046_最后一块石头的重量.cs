8f9ebfb789bd35bec77a0ce7bf2f320c83a00002
Jim Li <lij42@airproducts.com>
2020-05-29 20:18:53 +0800

    1046. 最后一块石头的重量 1221. 分割平衡字符串

---
+++ LeetCode/01046_最后一块石头的重量.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+/// <summary>
+/// 1046. 最后一块石头的重量
+/// https://leetcode-cn.com/problems/last-stone-weight/
+/// </summary>
+namespace Algorithym.LeetCode
+{
+    public class _01046_最后一块石头的重量
+    {
+        public int LastStoneWeight(int[] stones)
+        {
+            if (stones == null || stones.Length == 0) return 0;
+            int pre = 0, i = 1000;
+            int[] weight = new int[1001];
+            for (int n = 0; n < stones.Length; n++) weight[stones[n]]++;
+            while (i > 0)
+            {
+                if (weight[i] == 0 || pre == 0 && (weight[i] & 1) == 0)
+                    i--;
+                else
+                {
+                    if (pre == 0)
+                    {
+                        weight[i] = 0;
+                        pre = i--;
+                    }
+                    else
+                    {
+                        weight[i]--;
+                        weight[pre - i]++;
+                        i = Math.Max(pre - i, i);
+                        pre = 0;
+                    }
+                }
+            }
+            return pre;
+        }
+    }
+}
---
+++ LeetCode/贪心算法/分割平衡字符串.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+/// <summary>
+/// 1221. 分割平衡字符串
+/// https://leetcode-cn.com/problems/split-a-string-in-balanced-strings/
+/// </summary>
+namespace Algorithym.LeetCode.贪心算法
+{
+    public class _01221_分割平衡字符串
+    {
+        public int BalancedStringSplit(string s)
+        {
+            if (string.IsNullOrEmpty(s)) return 0;
+            int cntL = 0, cntR = 0, count = 0;
+            for (int i = 0; i < s.Length; i++)
+            {
+                if (s[i] == 'L')
+                    cntL++;
+                else
+                    cntR++;
+
+                if (cntL == cntR)
+                {
+                    count++;
+                    cntL = 0;
+                    cntR = 0;
+                }
+            }
+            return count;
+        }
+    }
+}
