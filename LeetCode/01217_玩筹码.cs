aa42ccff47729566b422f4e9cb6de3eee8ec18e2
Jim Li <lij42@airproducts.com>
2020-05-30 14:14:53 +0800

    1217. 玩筹码

---
+++ LeetCode/01217_玩筹码.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+/// <summary>
+/// 1217. 玩筹码
+/// https://leetcode-cn.com/problems/play-with-chips/
+/// </summary>
+namespace Algorithym.LeetCode
+{
+    public class _01217_玩筹码
+    {
+        public int MinCostToMoveChips(int[] chips)
+        {
+            int even = 0, odd = 0;
+            foreach (int n in chips)
+            {
+                if ((n & 1) == 0)
+                    even++;
+                else
+                    odd++;
+            }
+            return Math.Min(even, odd);
+        }
+    }
+}
