1e1b9896d9904a31fbd40daf083ab0ee05d775e4
Jim Li <lij42@airproducts.com>
2020-05-31 11:06:50 +0800

    1370. 上升下降字符串

---
+++ LeetCode/01370_上升下降字符串.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+/// <summary>
+/// 1370. 上升下降字符串
+/// https://leetcode-cn.com/problems/increasing-decreasing-string/
+/// </summary>
+namespace Algorithym.LeetCode
+{
+    public class _01370_上升下降字符串
+    {
+        public string SortString(string s)
+        {
+            int len = s.Length, pos = 0;
+            char[] result = new char[len];
+            int[] data = new int[123];
+            for (int n = 0; n < len; n++) data[s[n]]++;
+            while (pos < len)
+            {
+                for (int i = 97; i < 123; i++)
+                {
+                    if (pos == len) break;
+                    if (data[i] == 0) continue;
+                    result[pos++] = (char)i;
+                    data[i]--;
+                }
+                for (int i = 122; i >= 97; i--)
+                {
+                    if (pos == len) break;
+                    if (data[i] == 0) continue;
+                    result[pos++] = (char)i;
+                    data[i]--;
+                }
+            }
+
+            return new String(result);
+        }
+    }
+}
