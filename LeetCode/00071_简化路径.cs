b7b2bd816157ea8652ff50148c27e5e6118ef0bf
Jim Li <lij42@airproducts.com>
2020-06-02 21:23:12 +0800

    71. 简化路径

---
+++ LeetCode/00071_简化路径.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+/// <summary>
+/// 71. 简化路径
+/// https://leetcode-cn.com/problems/simplify-path/
+/// </summary>
+namespace Algorithym.LeetCode
+{
+    public class _00071_简化路径
+    {
+        Stack<string> s;
+        StringBuilder sBuilder;
+        public string SimplifyPath(string path)
+        {
+            if (string.IsNullOrEmpty(path)) return "";
+            s = new Stack<string>();
+            sBuilder = new StringBuilder();
+            foreach (char c in path)
+            {
+                if (c == '/')
+                    check();
+                else
+                    sBuilder.Append(c);
+            }
+            check();
+            return "/" + string.Join("/", s.ToArray().Reverse());
+        }
+
+        public void check()
+        {
+            if (sBuilder.Length != 0)
+            {
+                string cur = sBuilder.ToString();
+                if (cur != ".")
+                {
+                    if (cur == "..")
+                    {
+                        if (s.Count > 0) s.Pop();
+                    }
+                    else
+                        s.Push(cur);
+                }
+                sBuilder.Clear();
+            }
+        }
+    }
+}
