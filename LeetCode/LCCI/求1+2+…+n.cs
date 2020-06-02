78c6add9169ee5061b370c7ba3f26cb8190434fa
Jim Li <lij42@airproducts.com>
2020-06-02 08:11:19 +0800

    面试题64. 求1+2+…+n

---
+++ LeetCode/LCCI/求1+2+…+n.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+/// <summary>
+/// 面试题64. 求1+2+…+n
+/// https://leetcode-cn.com/problems/qiu-12n-lcof/
+/// </summary>
+namespace Algorithym.LeetCode.LCCI
+{
+    public class 求1_2___n
+    {
+        public int result;
+        public int SumNums(int n)
+        {
+            helper(n);
+            return result;
+        }
+
+        public bool helper(int n)
+        {
+            result += n;
+            return n == 0 || helper(n - 1);
+        }
+    }
+}
