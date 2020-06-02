b53123529cd847399255eaf350116c928f54d947
Jim Li <lij42@airproducts.com>
2020-06-02 20:51:35 +0800

    61. 旋转链表

---
+++ LeetCode/00061_旋转链表.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+/// <summary>
+/// 61. 旋转链表
+/// https://leetcode-cn.com/problems/rotate-list/
+/// </summary>
+namespace Algorithym.LeetCode
+{
+    public class _00061_旋转链表
+    {
+        public ListNode RotateRight(ListNode head, int k)
+        {
+            if (k == 0 || head == null) return head;
+            int count = 0;
+            ListNode dumHead = new ListNode(0), fast = dumHead, slow = dumHead;
+            dumHead.next = head;
+            while (fast != null && fast.next != null)
+            {
+                fast = fast.next.next;
+                slow = slow.next;
+                count++;
+            }
+            count = fast == null ? (count << 1) - 1 : (count << 1);
+            k %= count;
+
+            if (k == 0) return head;
+            fast = dumHead;
+            slow = dumHead;
+            while (k-- > 0) fast = fast.next;
+            while (fast.next != null)
+            {
+                fast = fast.next;
+                slow = slow.next;
+            }
+            ListNode newHead = slow.next;
+            slow.next = null;
+            fast.next = dumHead.next;
+            return newHead;
+        }
+    }
+}
