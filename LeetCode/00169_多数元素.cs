2a1b6f5e331796c7478783cbda6ad0d3932466a4
Jim Li <lij42@airproducts.com>
2020-06-05 19:44:27 +0800

    169. 多数元素

---
+++ LeetCode/00169_多数元素.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+/// <summary>
+/// 169. 多数元素
+/// https://leetcode-cn.com/problems/majority-element/
+/// </summary>
+namespace Algorithym.LeetCode
+{
+    public class _00169_多数元素
+    {
+        public int MajorityElement(int[] nums)
+        {
+            if (nums == null || nums.Length == 0)
+                return -1;
+            Array.Sort(nums);
+            return nums[nums.Length >> 1];
+        }
+    }
+}
