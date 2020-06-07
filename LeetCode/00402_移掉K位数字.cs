b81b832d05594d9bd5f64e2b53fae2b0faa5993c
Jim Li <lij42@airproducts.com>
2020-06-07 19:03:28 +0800

    File changes.

---
+++ LeetCode/00402_移掉K位数字.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+/// <summary>
+/// 402. 移掉K位数字
+/// https://leetcode-cn.com/problems/remove-k-digits/
+/// </summary>
+namespace Algorithym.LeetCode
+{
+    public class _00402_移掉K位数字
+    {
+        public string RemoveKdigits(string num, int k)
+        {
+            if (string.IsNullOrEmpty(num)) return "";
+            if (num.Length == k) return "0";
+            if (k == 0) return num;
+
+            int len = num.Length;
+            List<char> data = new List<char>(num.ToCharArray());
+            while (k > 0 && len > 0)
+            {
+                if (data[0] == '0')
+                {
+                    data.RemoveAt(0);
+                    len--;
+                }
+                int pos = len - 1;
+                for (int i = 1; i < len; i++)
+                {
+                    if (data[i] < data[i - 1])
+                    {
+                        pos = i - 1;
+                        break;
+                    }
+                }
+                data.RemoveAt(pos);
+                len--;
+                k--;
+            }
+            string result = new String(data.ToArray()).TrimStart('0');
+            return result == "" ? "0" : result;
+        }
+    }
+}
---
+++ LeetCode/贪心算法/单调递增的数字.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+/// <summary>
+/// 738. 单调递增的数字
+/// https://leetcode-cn.com/problems/monotone-increasing-digits/
+/// </summary>
+namespace Algorithym.LeetCode.贪心算法
+{
+    public class _00738_单调递增的数字
+    {
+        public int MonotoneIncreasingDigits(int N)
+        {
+            if (N <= 0) return 0;
+            if (N < 10) return N;
+
+            StringBuilder num = new StringBuilder(N.ToString());
+            int result = 0, pos = -1;
+            for (int i = 1; i < num.Length; i++)
+            {
+                if (num[i] - '0' < num[i - 1] - '0')
+                {
+                    pos = i - 1;
+                    break;
+                }
+            }
+            if (pos == -1)
+                return N;
+            else
+            {
+                int index = 0;
+                for (int i = pos; i > 0; i--)
+                {
+                    if ((num[i] - '0') - (num[i - 1] - '0') > 0)
+                    {
+                        index = i;
+                        break;
+                    }
+                }
+                if (index == 0 && num[index] == '1')
+                {
+                    num = num.Remove(index, 1);
+                    num[index] = '9';
+                }
+                else
+                    num[index]--;
+                for (int i = index + 1; i < num.Length; i++) num[i] = '9';
+                return int.Parse(num.ToString());
+            }
+        }
+    }
+}
