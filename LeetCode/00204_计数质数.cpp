8150441a1ace015c0941e027aeccb577564026d0
Jim Li <lij42@airproducts.com>
2020-06-08 20:50:03 +0800

    204. 计数质数

---
+++ LeetCode/00204_计数质数.cpp
@@ -0,0 +1,20 @@
+﻿/*
+204. 计数质数
+https://leetcode-cn.com/problems/count-primes/
+*/
+class Solution {
+public:
+    int countPrimes(int n) {
+        if (n < 1) return n;
+        int result = 0;
+        bool data[n];
+        for (int i = 0; i < n; i++) data[i] = true;
+        for (int i = 2; i * i < n; i++)
+        {
+            if (data[i])
+                for (int j = 2 * i; j < n; j += i) data[j] = false;
+        }
+        for (int i = 2; i < n; i++) result += data[i] ? 1 : 0;
+        return result;
+    }
+};
---
+++ LeetCode/数学/超级丑数.cpp
@@ -0,0 +1,30 @@
+﻿/*
+313. 超级丑数
+https://leetcode-cn.com/problems/super-ugly-number/
+*/
+class Solution
+{
+public:
+    int nthSuperUglyNumber(int n, vector<int>& primes)
+    {
+        if (n < 2) return n;
+        int k = primes.size(), p[k], result[n];
+        result[0] = 1;
+        for (int i = 0; i < k; i++) p[i] = 0;
+        for (int i = 1; i < n; i++)
+        {
+            int min = INT_MAX, v[k];
+            for (int j = 0; j < k; j++)
+            {
+                v[j] = result[p[j]] * primes[j];
+                if (v[j] < min) min = v[j];
+            }
+            result[i] = min;
+            for (int j = 0; j < k; j++)
+            {
+                if (min == v[j]) p[j]++;
+            }
+        }
+        return result[n - 1];
+    }
+};
