f9cf7c471fb6b8e84e9221dd3a5a30b8164c8d72
Jim Li <lij42@airproducts.com>
2020-06-08 14:05:12 +0800

    202. 快乐数

---
+++ LeetCode/00202_快乐数.cpp
@@ -0,0 +1,23 @@
+﻿/*
+202. 快乐数
+https://leetcode-cn.com/problems/happy-number/
+*/
+class Solution {
+public:
+    bool isHappy(int n) {
+        unordered_set<int> map;
+        while (n != 1)
+        {
+            int result = 0;
+            while (n != 0)
+            {
+                result += pow(n % 10, 2);
+                n /= 10;
+            }
+            n = result;
+            if (map.find(n) != map.end()) return false;
+            map.insert(n);
+        }
+        return true;
+    }
+};
