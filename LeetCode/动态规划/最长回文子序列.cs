5e629a631a6c2187b8b59178e1513739c34b8127
Jim Li <lij42@airproducts.com>
2020-06-10 13:25:01 +0800

    516. 最长回文子序列

---
+++ LeetCode/动态规划/最长回文子序列.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+/// <summary>
+/// 516. 最长回文子序列
+/// https://leetcode-cn.com/problems/longest-palindromic-subsequence/
+/// </summary>
+namespace Algorithym.LeetCode.动态规划
+{
+    public class _00516_最长回文子序列
+    {
+        public int LongestPalindromeSubseq(string s)
+        {
+            int len = s.Length;
+            int[,] dp = new int[len, len];
+            for (int i = len - 1; i >= 0; i--)
+            {
+                dp[i, i] = 1;
+                for (int j = i + 1; j < len; j++)
+                {
+                    if (s[i] == s[j])
+                        dp[i, j] = dp[i + 1, j - 1] + 2;
+                    else
+                        dp[i, j] = Math.Max(dp[i + 1, j], dp[i, j - 1]);
+                }
+            }
+            return dp[0, len - 1];
+        }
+    }
+}
