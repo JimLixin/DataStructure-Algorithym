72e2ca0ec4b9e74d0ae02689840e254e5ed92f2f
Jim Li <lij42@airproducts.com>
2020-06-11 19:15:18 +0800

    739. 每日温度

---
+++ LeetCode/栈/每日温度.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+/// <summary>
+/// 739. 每日温度
+/// https://leetcode-cn.com/problems/daily-temperatures/
+/// </summary>
+namespace Algorithym.LeetCode.栈
+{
+    public class _00739_每日温度
+    {
+        public int[] DailyTemperatures(int[] T)
+        {
+            int len = T.Length;
+            int[] ans = new int[len];
+            Stack<int> s = new Stack<int>();
+            for (int i = 0; i < len; i++)
+            {
+                while (s.Count > 0 && T[s.Peek()] < T[i])
+                {
+                    int pre = s.Pop();
+                    ans[pre] = i - pre;
+                }
+                s.Push(i);
+            }
+            return ans;
+        }
+    }
+}
