25fdf927096384f0cd74485e8b1a538dcd95c67e
Jim Li <lij42@airproducts.com>
2020-06-12 13:45:08 +0800

    162. 寻找峰值

---
+++ LeetCode/00162_寻找峰值.cpp
@@ -0,0 +1,20 @@
+﻿/*
+162. 寻找峰值
+https://leetcode-cn.com/problems/find-peak-element/
+*/
+class Solution {
+public:
+    int findPeakElement(vector<int>& nums) {
+        return helper(nums, 0, nums.size() - 1);
+    }
+
+    int helper(vector<int>& nums, int l, int r)
+    {
+        if (l == r) return l;
+        int mid = l + (r - l) / 2;
+        if (nums[mid] > nums[mid + 1])
+            return helper(nums, l, mid);
+        else
+            return helper(nums, mid + 1, r);
+    }
+};
