b95ef0496201bfcaf7859314b79b5c49bd97b108
Jim Li <lij42@airproducts.com>
2020-06-12 15:47:35 +0800

    面试题36. 二叉搜索树与双向链表

---
+++ LeetCode/LCOF/二叉搜索树与双向链表.cpp
@@ -0,0 +1,62 @@
+﻿/*
+// Definition for a Node.
+class Node {
+public:
+    int val;
+    Node* left;
+    Node* right;
+
+    Node() {}
+
+    Node(int _val) {
+        val = _val;
+        left = NULL;
+        right = NULL;
+    }
+
+    Node(int _val, Node* _left, Node* _right) {
+        val = _val;
+        left = _left;
+        right = _right;
+    }
+};
+*/
+
+
+/*
+面试题36. 二叉搜索树与双向链表
+https://leetcode-cn.com/problems/er-cha-sou-suo-shu-yu-shuang-xiang-lian-biao-lcof/
+*/
+class Solution {
+public:
+    Node* treeToDoublyList(Node* root) {
+        if (!root) return root;
+        stack<Node*> s;
+        Node* node = root, * head = nullptr, * pre = nullptr;
+        while (!s.empty() || node != NULL)
+        {
+            while (node != NULL)
+            {
+                s.push(node);
+                node = node->left;
+            }
+            node = s.top();
+            s.pop();
+            if (pre == nullptr)
+            {
+                pre = node;
+                head = node;
+            }
+            else
+            {
+                pre->right = node;
+                node->left = pre;
+                pre = node;
+            }
+            node = node->right;
+        }
+        pre->right = head;
+        head->left = pre;
+        return head;
+    }
+};
--- LeetCode/动态规划/最长等差数列.cs
+++ LeetCode/动态规划/最长等差数列.cs
@@ -43,4 +43,35 @@ namespace Algorithym.LeetCode.动态规划
             return max;
         }
     }
+
+    //C++版本
+    /*
+    class Solution {
+    public:
+        int longestArithSeqLength(vector<int>& A) {
+            int len = A.size(), max = 2;
+            unordered_map<int, vector<int>> dic;
+            vector<vector<int>> dp(len, vector<int>(len));
+            for(int i = 0; i < len; i++) dic[A[i]].push_back(i);
+            for(int i = 0; i < len; i++)
+            {
+                dp[i][i] = 1;
+                for(int j = i+1; j < len; j++)
+                {
+                    dp[i][j] = 2;
+                    int diff = A[i] * 2 - A[j];
+                    if(dic.find(diff) != dic.end())
+                    {
+                        for(auto idx : dic[diff])
+                        {
+                            if(idx < i) dp[i][j] = dp[idx][i] + 1;
+                        }
+                    } 
+                    if(dp[i][j] > max) max = dp[i][j];
+                }
+            }
+            return max;
+        }
+    };     
+    */
 }
