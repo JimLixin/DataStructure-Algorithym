098c490b54093be16db09f1063769bc79bd55c7a
Jim Li <lij42@airproducts.com>
2020-06-13 19:59:04 +0800

    面试题 05.06. 整数转换

---
+++ LeetCode/LCCI/整数转换.cpp
@@ -0,0 +1,16 @@
+﻿/*
+面试题 05.06. 整数转换
+https://leetcode-cn.com/problems/convert-integer-lcci/
+*/
+class Solution {
+public:
+    int convertInteger(int A, int B) {
+        unsigned int n = A ^ B, res = 0;
+        while (n != 0)
+        {
+            res++;
+            n &= n - 1;
+        }
+        return res;
+    }
+};
