96e4ce64b08823b1ca4c877d44bea0a20e95e75a
Jim Li <lij42@airproducts.com>
2020-06-13 19:11:11 +0800

    328. 奇偶链表

---
+++ LeetCode/00328_奇偶链表.cpp
@@ -0,0 +1,31 @@
+﻿/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+
+/*
+328. 奇偶链表
+https://leetcode-cn.com/problems/odd-even-linked-list/
+*/
+class Solution {
+public:
+    ListNode* oddEvenList(ListNode* head) {
+        if (head == nullptr) return head;
+        ListNode* p1 = head, * h2 = head->next, * p2 = h2;
+        while (p2 && p2->next)
+        {
+            p1->next = p2->next;
+            p1 = p2->next;
+            p2->next = p1->next;
+            p2 = p1->next;
+        }
+        p1->next = h2;
+        return head;
+    }
+};
