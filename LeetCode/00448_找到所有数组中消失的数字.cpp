8626d79ce53f11b93146594a183c53806fb39d5d
Jim Li <lij42@airproducts.com>
2020-06-14 14:07:01 +0800

    448. 找到所有数组中消失的数字

---
+++ LeetCode/00448_找到所有数组中消失的数字.cpp
@@ -0,0 +1,29 @@
+﻿/*
+448. 找到所有数组中消失的数字
+https://leetcode-cn.com/problems/find-all-numbers-disappeared-in-an-array/
+*/
+class Solution {
+
+public:
+    vector<int> findDisappearedNumbers(vector<int>& nums) {
+        int pos = 0, n = nums.size();
+        vector<int> ans;
+        while (pos < n)
+        {
+            if (nums[pos] == pos + 1 || nums[pos] == nums[nums[pos] - 1])
+            {
+                pos++;
+                continue;
+            }
+            int tmp1 = nums[pos], tmp2 = nums[nums[pos] - 1];
+            nums[nums[pos] - 1] = tmp1;
+            nums[pos] = tmp2;
+        }
+        for (int i = 0; i < n; i++)
+        {
+            if (nums[i] != i + 1)
+                ans.push_back(i + 1);
+        }
+        return ans;
+    }
+};
