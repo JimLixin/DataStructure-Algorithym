e1f73da12c75f1160818fb08d5e34b4e95d91d6c
Jim Li <lij42@airproducts.com>
2020-06-14 14:18:09 +0800

    617. 合并二叉树

---
+++ LeetCode/00617_合并二叉树.cpp
@@ -0,0 +1,26 @@
+﻿/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+ * };
+ */
+
+/*
+617. 合并二叉树
+https://leetcode-cn.com/problems/merge-two-binary-trees/
+*/
+class Solution {
+public:
+    TreeNode* mergeTrees(TreeNode* t1, TreeNode* t2) {
+        if (t1 == nullptr && t2 == nullptr) return NULL;
+        int v = (t1 == nullptr ? 0 : t1->val) + (t2 == nullptr ? 0 : t2->val);
+        TreeNode* node = t1 == nullptr ? t2 : t1;
+        node->val = v;
+        node->left = mergeTrees(t1 == nullptr ? NULL : t1->left, t2 == nullptr ? NULL : t2->left);
+        node->right = mergeTrees(t1 == nullptr ? NULL : t1->right, t2 == nullptr ? NULL : t2->right);
+        return node;
+    }
+};
