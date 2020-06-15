31dd9486a3d670ef357fc5daae07754d1e585f7b
Jim Li <lij42@airproducts.com>
2020-06-15 19:58:42 +0800

    面试题 16.02. 单词频率

---
+++ LeetCode/LCCI/单词频率.cpp
@@ -0,0 +1,22 @@
+﻿/*
+面试题 16.02. 单词频率
+https://leetcode-cn.com/problems/words-frequency-lcci/
+*/
+class WordsFrequency {
+private:
+    unordered_map<string, int> dic;
+public:
+    WordsFrequency(vector<string>& book) {
+        for (auto word : book) dic[word]++;
+    }
+
+    int get(string word) {
+        return dic[word];
+    }
+};
+
+/**
+ * Your WordsFrequency object will be instantiated and called as such:
+ * WordsFrequency* obj = new WordsFrequency(book);
+ * int param_1 = obj->get(word);
+ */
