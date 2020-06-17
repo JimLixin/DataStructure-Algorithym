2a4e6b78c2b3971ffa0bdb0bf03e0bdc062932a4
Jim Li <lij42@airproducts.com>
2020-06-17 21:35:41 +0800

    1281. 整数的各位积和之差

---
+++ LeetCode/01281_整数的各位积和之差.cpp
@@ -0,0 +1,18 @@
+﻿/*
+1281. 整数的各位积和之差
+https://leetcode-cn.com/problems/subtract-the-product-and-sum-of-digits-of-an-integer/
+*/
+class Solution {
+public:
+    int subtractProductAndSum(int n) {
+        int k = 0, a = 0, b = 1;
+        while (n != 0)
+        {
+            k = n % 10;
+            a += k;
+            b *= k;
+            n /= 10;
+        }
+        return b - a;
+    }
+};
