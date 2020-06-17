f2ff3989d4574363a4e81a2cd749ad7802c32713
Jim Li <lij42@airproducts.com>
2020-06-17 12:35:37 +0800

    1014. 最佳观光组合

---
+++ LeetCode/01014_最佳观光组合.cpp
@@ -0,0 +1,16 @@
+﻿/*
+1014. 最佳观光组合
+https://leetcode-cn.com/problems/best-sightseeing-pair/
+*/
+class Solution {
+public:
+    int maxScoreSightseeingPair(vector<int>& A) {
+        int a = A[0], b = 0;
+        for (int i = 1; i < A.size(); i++)
+        {
+            b = max(b, a + A[i] - i);
+            a = max(a, A[i] + i);
+        }
+        return b;
+    }
+};
