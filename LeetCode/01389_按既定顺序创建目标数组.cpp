794c5ab7577b53f4eefd35088dcab140ba22eec3
Jim Li <lij42@airproducts.com>
2020-06-18 15:27:06 +0800

    1389. 按既定顺序创建目标数组

---
+++ LeetCode/01389_按既定顺序创建目标数组.cpp
@@ -0,0 +1,16 @@
+﻿/*
+1389. 按既定顺序创建目标数组
+https://leetcode-cn.com/problems/create-target-array-in-the-given-order/
+*/
+class Solution {
+public:
+    vector<int> createTargetArray(vector<int>& nums, vector<int>& index) {
+        int n = nums.size();
+        vector<int> res;
+        for (int i = 0; i < n; i++)
+        {
+            res.insert(res.begin() + index[i], nums[i]);
+        }
+        return res;
+    }
+};
