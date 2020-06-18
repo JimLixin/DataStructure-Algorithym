7699ccc01e3a60372dff2c61f90af2590bbc0314
Jim Li <lij42@airproducts.com>
2020-06-18 13:59:36 +0800

    1470. 重新排列数组

---
+++ LeetCode/01470_重新排列数组.cpp
@@ -0,0 +1,17 @@
+﻿/*
+1470. 重新排列数组
+https://leetcode-cn.com/problems/shuffle-the-array/
+*/
+class Solution {
+public:
+    vector<int> shuffle(vector<int>& nums, int n) {
+        int len = nums.size(), k = len / 2;;
+        vector<int> res = vector<int>(len);
+        for (int i = 0; i < k; i++)
+        {
+            res[2 * i] = nums[i];
+            res[2 * i + 1] = nums[i + k];
+        }
+        return res;
+    }
+};
