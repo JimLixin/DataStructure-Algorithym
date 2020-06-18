188be1d6c2df4e36375aa40ca44a87761d0ab50a
Jim Li <lij42@airproducts.com>
2020-06-18 16:58:09 +0800

    1290. 二进制链表转整数

---
+++ LeetCode/01290_二进制链表转整数.cpp
@@ -0,0 +1,25 @@
+﻿/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode(int x) : val(x), next(NULL) {}
+ * };
+ */
+
+/*
+1290. 二进制链表转整数
+https://leetcode-cn.com/problems/convert-binary-number-in-a-linked-list-to-integer/
+*/
+class Solution {
+public:
+    int getDecimalValue(ListNode* head) {
+        int res = 0;
+        while (head != nullptr)
+        {
+            res = res * 2 + head->val;
+            head = head->next;
+        }
+        return res;
+    }
+};
