ee7dd1c4787d77036468ad0393fc1d47df6949e5
Jim Li <lij42@airproducts.com>
2020-06-18 15:00:04 +0800

    1108. IP 地址无效化

---
+++ LeetCode/01108_IP 地址无效化.cpp
@@ -0,0 +1,15 @@
+﻿/*
+1108. IP 地址无效化
+https://leetcode-cn.com/problems/defanging-an-ip-address/
+*/
+class Solution {
+public:
+    string defangIPaddr(string address) {
+        string res;
+        for (auto c : address)
+        {
+            res += isdigit(c) ? to_string(c - 48) : "[.]";
+        }
+        return res;
+    }
+};
