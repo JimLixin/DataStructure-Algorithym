4c015360aec8779e8b36aa2b932174e6a5c567ea
Jim Li <lij42@airproducts.com>
2020-06-19 19:53:35 +0800

    188. 买卖股票的最佳时机 IV

--- LeetCode/00123_买卖股票的最佳时机_III.cpp
+++ LeetCode/00123_买卖股票的最佳时机_III.cpp
@@ -8,22 +8,20 @@ public:
         int n = prices.size();
         if (n < 2) return 0;
 
-        vector<vector<vector<int>>> dp(n, vector<vector<int>>(2, vector<int>(3, 0)));
+        vector<vector<vector<long>>> dp(n, vector<vector<long>>(2, vector<long>(3, 0)));
         dp[0][0][0] = 0;
         dp[0][1][0] = -prices[0];
-        dp[0][0][1] = 0;
-        dp[0][0][2] = 0;
+        dp[0][0][1] = INT_MIN;
+        dp[0][0][2] = INT_MIN;
         dp[0][1][1] = INT_MIN;
         dp[0][1][2] = INT_MIN;
         for (int i = 1; i < n; i++)
         {
-            dp[i][0][0] = 0;
-            dp[i][0][1] = max(dp[i - 1][0][1], dp[i - 1][1][0] + prices[i]);
-            dp[i][0][2] = max(dp[i - 1][0][2], dp[i - 1][1][1] + prices[i]);
-            dp[i][1][0] = max(dp[i - 1][1][0], -prices[i]);
-            dp[i][1][1] = max(dp[i - 1][1][1], dp[i - 1][0][1] - prices[i]);
-            dp[i][1][2] = INT_MIN;
+            dp[i][0][1] = max<long>(dp[i - 1][0][1], dp[i - 1][1][0] + prices[i]);
+            dp[i][0][2] = max<long>(dp[i - 1][0][2], dp[i - 1][1][1] + prices[i]);
+            dp[i][1][0] = max<long>(dp[i - 1][1][0], -prices[i]);
+            dp[i][1][1] = max<long>(dp[i - 1][1][1], dp[i - 1][0][1] - prices[i]);
         }
-        return max(0, max(dp[n - 1][0][1], dp[n - 1][0][2]));
+        return max<long>(0, max<long>(dp[n - 1][0][1], dp[n - 1][0][2]));
     }
 };
---
+++ LeetCode/动态规划/买卖股票的最佳时机_IV.cpp
@@ -0,0 +1,47 @@
+﻿/*
+188. 买卖股票的最佳时机 IV
+https://leetcode-cn.com/problems/best-time-to-buy-and-sell-stock-iv/
+*/
+class Solution {
+public:
+    int maxProfit(int k, vector<int>& prices) {
+        int n = prices.size();
+        long profit = 0;
+        if (n < 2) return 0;
+        if (k > n / 2) return maxProfitOfII(prices);
+        vector<vector<vector<long>>> dp(n, vector<vector<long>>(2, vector<long>(k + 1, 0)));
+        for (int i = 0; i < k; i++)
+        {
+            if (i == 0)
+            {
+                dp[0][0][i] = 0;
+                dp[0][1][i] = -prices[0];
+            }
+            else
+            {
+                dp[0][0][i] = INT_MIN;
+                dp[0][1][i] = INT_MIN;
+            }
+        }
+        for (int i = 1; i < n; i++)
+        {
+            for (int j = 0; j <= k; j++)
+            {
+                dp[i][0][j] = j == 0 ? 0 : max<long>(dp[i - 1][0][j], dp[i - 1][1][j - 1] + prices[i]);
+                dp[i][1][j] = j == k ? INT_MIN : max<long>(dp[i - 1][1][j], dp[i - 1][0][j] - prices[i]);
+                if (i == n - 1 && dp[i][0][j] > profit) profit = dp[i][0][j];
+            }
+        }
+        return profit;
+    }
+
+    int maxProfitOfII(vector<int>& prices) {
+        int res = 0;
+        for (int i = 1; i < prices.size(); i++) {
+            if (prices[i] > prices[i - 1]) {
+                res += prices[i] - prices[i - 1];
+            }
+        }
+        return res;
+    }
+};
