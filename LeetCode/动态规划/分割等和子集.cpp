39a24c802202166a6ed819c8e1011b02d8eed34f
Jim Li <lij42@airproducts.com>
2020-06-20 20:25:21 +0800

    416. 分割等和子集

---
+++ LeetCode/动态规划/分割等和子集.cpp
@@ -0,0 +1,25 @@
+﻿/*
+416. 分割等和子集
+https://leetcode-cn.com/problems/partition-equal-subset-sum/
+*/
+class Solution {
+public:
+    bool canPartition(vector<int>& nums) {
+        int n = nums.size(), sum = accumulate(nums.begin(), nums.end(), 0);
+        if ((sum & 1) > 0) return false;
+        sum >>= 1;
+        vector<vector<bool>> dp(n + 1, vector<bool>(sum + 1, false));
+        for (int i = 0; i < n; i++) dp[i][0] = true;
+        for (int i = 1; i <= n; i++)
+        {
+            for (int j = 1; j <= sum; j++)
+            {
+                if (j < nums[i - 1])
+                    dp[i][j] = dp[i - 1][j];
+                else
+                    dp[i][j] = dp[i - 1][j] || dp[i - 1][j - nums[i - 1]];
+            }
+        }
+        return dp[n][sum];
+    }
+};
