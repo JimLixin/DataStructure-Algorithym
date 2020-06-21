1c57a64298196191605ecc388e0743d56ebea834
Jim Li <lij42@airproducts.com>
2020-06-21 10:19:38 +0800

    172. 阶乘后的零

---
+++ LeetCode/00172_阶乘后的零.cpp
@@ -0,0 +1,19 @@
+﻿/*
+172. 阶乘后的零
+https://leetcode-cn.com/problems/factorial-trailing-zeroes/
+*/
+class Solution {
+public:
+    int trailingZeroes(int n) {
+        //return n == 0 ? 0 : n/5 + trailingZeroes(n/5);
+
+        int res = 0;
+        while (n > 0)
+        {
+            n = n / 5;
+            res += n;
+        }
+
+        return res;
+    }
+};
