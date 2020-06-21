f82739b342ff8f00703b57819cf8f41dfc14b3cf
Jim Li <lij42@airproducts.com>
2020-06-21 14:45:19 +0800

    290. 单词规律

---
+++ LeetCode/00290_单词规律.cpp
@@ -0,0 +1,27 @@
+﻿/*
+290. 单词规律
+https://leetcode-cn.com/problems/word-pattern/
+*/
+class Solution {
+public:
+    bool wordPattern(string pattern, string str) {
+        int i = 0;
+        unordered_map<string, char> map;
+        unordered_map<char, string> map2;
+        string word;
+        stringstream ss(str);
+        while (getline(ss, word, ' '))
+        {
+            if (i == pattern.size()) return false;
+            if (map.count(word) && map[word] != pattern[i] || map2.count(pattern[i]) && map2[pattern[i]] != word)
+                return false;
+            else
+            {
+                map[word] = pattern[i];
+                map2[pattern[i]] = word;
+                i++;
+            }
+        }
+        return i == pattern.size();
+    }
+};
