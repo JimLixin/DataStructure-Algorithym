7d36be277cfcd8c3677144e6974cc62766d3508c
Jim Li <lij42@airproducts.com>
2020-06-22 14:45:23 +0800

    876. 链表的中间结点

---
+++ LeetCode/00876_链表的中间结点.cpp
@@ -0,0 +1,25 @@
+﻿/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode(int x) : val(x), next(NULL) {}
+ * };
+ */
+
+/*
+876. 链表的中间结点
+https://leetcode-cn.com/problems/middle-of-the-linked-list/
+*/
+class Solution {
+public:
+    ListNode* middleNode(ListNode* head) {
+        ListNode* fast = head, * slow = head;
+        while (fast != nullptr && fast->next != nullptr)
+        {
+            fast = fast->next->next;
+            slow = slow->next;
+        }
+        return slow;
+    }
+};
