4f37b924138ed238470f2bde64173d9310774677
Jim Li <lij42@airproducts.com>
2020-06-23 09:55:13 +0800

    429. N叉树的层序遍历

---
+++ LeetCode/00429_N叉树的层序遍历.cpp
@@ -0,0 +1,48 @@
+﻿/*
+// Definition for a Node.
+class Node {
+public:
+    int val;
+    vector<Node*> children;
+
+    Node() {}
+
+    Node(int _val) {
+        val = _val;
+    }
+
+    Node(int _val, vector<Node*> _children) {
+        val = _val;
+        children = _children;
+    }
+};
+*/
+
+
+/*
+429. N叉树的层序遍历
+https://leetcode-cn.com/problems/n-ary-tree-level-order-traversal/
+*/
+class Solution {
+public:
+    vector<vector<int>> levelOrder(Node* root) {
+        if (root == nullptr) return {};
+        vector<vector<int>> res;
+        queue<Node*> q;
+        q.push(root);
+        while (!q.empty())
+        {
+            int cnt = q.size();
+            vector<int> data(cnt);
+            for (int i = 0; i < cnt; i++)
+            {
+                Node* tmp = q.front();
+                q.pop();
+                data[i] = tmp->val;
+                for (auto c : tmp->children) q.push(c);
+            }
+            res.push_back(data);
+        }
+        return res;
+    }
+};
