072105d0a5377d9c5c1a6c8aa5e56f9b002cf525
Jim Li <lij42@airproducts.com>
2020-06-23 14:22:26 +0800

    654. 最大二叉树

---
+++ LeetCode/00654_最大二叉树.cpp
@@ -0,0 +1,31 @@
+﻿/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+ * };
+ */
+
+/*
+654. 最大二叉树
+https://leetcode-cn.com/problems/maximum-binary-tree/
+*/
+class Solution {
+public:
+    TreeNode* constructMaximumBinaryTree(vector<int>& nums) {
+        if (nums.empty()) return nullptr;
+        return helper(nums, nums.begin(), nums.end());
+    }
+
+    TreeNode* helper(vector<int>& nums, vector<int>::iterator start, vector<int>::iterator end)
+    {
+        if (start >= end) return nullptr;
+        auto maxPos = max_element(start, end);
+        TreeNode* root = new TreeNode(*maxPos);
+        root->left = helper(nums, start, maxPos);
+        root->right = helper(nums, maxPos + 1, end);
+        return root;
+    }
+};
