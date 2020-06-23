f9ce59af3603f732b6a1d4bffd67a28b40573572
Jim Li <lij42@airproducts.com>
2020-06-23 09:46:56 +0800

    589. N叉树的前序遍历

--- LeetCode/00067_二进制求和.cs
+++ LeetCode/00067_二进制求和.cs
@@ -3,12 +3,13 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-
+/// <summary>
+/// 67. 二进制求和
+/// https://leetcode-cn.com/problems/add-binary/
+/// </summary>
 namespace Algorithym.LeetCode
 {
-    /// <summary>
-    /// https://leetcode.com/problems/add-binary/
-    /// </summary>
+
     public class add_binary
     {
         public string AddBinary(string a, string b)
@@ -31,4 +32,25 @@ namespace Algorithym.LeetCode
             return new String(flag > 0 ? (new char[] { '1' }).Concat(data).ToArray() : data);
         }
     }
+    /*C++实现*/
+    /*
+     class Solution {
+    public:
+        string addBinary(string a, string b) {
+            string res;
+            int p1 = a.size() - 1, p2 = b.size() - 1, x = 0, y = 0, carry = 0;
+            while(p1 >= 0 || p2 >= 0)
+            {
+                x = p1 >= 0 ? a[p1--] - 48 : 0;
+                y = p2 >= 0 ? b[p2--] - 48 : 0;
+                int tmp = carry + x + y;
+                res += tmp%2 + 48;
+                carry = tmp>>1;
+            }
+            if(carry > 0) res += '1';
+            reverse(res.begin(), res.end());
+            return res;
+        }
+};
+     */
 }
---
+++ LeetCode/00589_N叉树的前序遍历.cpp
@@ -0,0 +1,76 @@
+﻿/*
+// Definition for a Node.
+class Node {
+public:
+    int val;
+    vector<Node*> children;
+
+    Node() {}
+
+    Node(int _val) {
+        val = _val;
+    }
+
+    Node(int _val, vector<Node*> _children) {
+        val = _val;
+        children = _children;
+    }
+};
+*/
+
+/*
+589. N叉树的前序遍历
+https://leetcode-cn.com/problems/n-ary-tree-preorder-traversal/
+*/
+
+/*
+递归版本
+*/
+class Solution {
+public:
+    vector<int> preorder(Node* root) {
+        if (root == nullptr) return {};
+        vector<int> res;
+        dfs(res, root);
+        return res;
+    }
+
+    void dfs(vector<int>& data, Node* node)
+    {
+        data.push_back(node->val);
+        for (auto child : node->children)
+        {
+            dfs(data, child);
+        }
+    }
+};
+
+/*
+迭代版本
+*/
+class Solution2 {
+public:
+    vector<int> preorder(Node* root) {
+        if (root == nullptr) return {};
+        vector<int> res;
+        Node* node = root;
+        stack<Node*> s;
+        while (!s.empty() || node != nullptr)
+        {
+            if (node == nullptr)
+            {
+                node = s.top();
+                s.pop();
+            }
+            res.push_back(node->val);
+            if (!node->children.empty())
+            {
+                for (int i = node->children.size() - 1; i > 0; i--) s.push(node->children[i]);
+                node = node->children[0];
+            }
+            else
+                node = nullptr;
+        }
+        return res;
+    }
+};
