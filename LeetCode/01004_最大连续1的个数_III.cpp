c953ad74dc259cdc2f0c266017ba32478b69ddcf
Jim Li <lij42@airproducts.com>
2020-06-24 13:39:54 +0800

    1004. 最大连续1的个数 III

---
+++ LeetCode/01004_最大连续1的个数_III.cpp
@@ -0,0 +1,20 @@
+﻿/*
+1004. 最大连续1的个数 III
+https://leetcode-cn.com/problems/max-consecutive-ones-iii/
+*/
+class Solution {
+public:
+    int longestOnes(vector<int>& A, int K) {
+        int left = 0, right = 0, maxCnt = 0, res = 0;
+        while (right < A.size())
+        {
+            if (A[right++] == 1) maxCnt++;
+            while (maxCnt + K < right - left)
+            {
+                if (A[left++] == 1) maxCnt--;
+            }
+            if (right - left > res) res = right - left;
+        }
+        return res;
+    }
+};
