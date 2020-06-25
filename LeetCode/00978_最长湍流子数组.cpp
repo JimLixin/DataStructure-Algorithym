cf623328a904b631f0ae10773ba78bf34c16c7b8
Jim Li <lij42@airproducts.com>
2020-06-25 14:14:55 +0800

    978. 最长湍流子数组

---
+++ LeetCode/00978_最长湍流子数组.cpp
@@ -0,0 +1,47 @@
+﻿/*
+978. 最长湍流子数组
+https://leetcode-cn.com/problems/longest-turbulent-subarray/
+*/
+class Solution {
+public:
+    //滑动窗口思路
+    int maxTurbulenceSize(vector<int>& A) {
+        int n = A.size();
+        if (n == 1) return 1;
+        if (n == 2) return A[0] == A[1] ? 1 : 2;
+        int left = 0, right = 2, res = A[0] == A[1] ? 1 : 2, flag = A[0] < A[1] ? -1 : 1;
+        while (right < A.size())
+        {
+            int tmp = (A[right - 1] - A[right]) * flag;
+            flag = A[right - 1] < A[right] ? -1 : 1;
+            if (tmp < 0)
+                res = max(res, right - left + 1);
+            else
+                left = right + (tmp > 0 ? -1 : 0);
+            right++;
+        }
+        return res;
+    }
+
+    //动态规划思路
+    int maxTurbulenceSizeV2(vector<int>& A) {
+        int n = A.size();
+        if (n == 1) return 1;
+        if (n == 2) return A[0] == A[1] ? 1 : 2;
+        vector<int> dp(n);
+        dp[0] = 1;
+        dp[1] = A[0] == A[1] ? 1 : 2;
+        int res = max(dp[0], dp[1]);
+        for (int i = 2; i < n; i++)
+        {
+            if (A[i - 2] < A[i - 1] && A[i - 1] > A[i] || A[i - 2] > A[i - 1] && A[i - 1] < A[i])
+            {
+                dp[i] = dp[i - 1] + 1;
+                res = max(res, dp[i]);
+            }
+            else
+                dp[i] = A[i - 1] == A[i] ? 1 : 2;
+        }
+        return res;
+    }
+};
---
+++ LeetCode/滑动窗口/可获得的最大点数.cpp
@@ -0,0 +1,33 @@
+﻿/*
+1423. 可获得的最大点数
+https://leetcode-cn.com/problems/maximum-points-you-can-obtain-from-cards/
+*/
+class Solution {
+public:
+    //滑动窗口思路
+    int maxScore(vector<int>& cardPoints, int k) {
+        int sum = accumulate(cardPoints.begin(), cardPoints.end(), 0);
+        if (k == cardPoints.size()) return sum;
+        int n = cardPoints.size(), left = 0, right = n - k - 1, curSum = accumulate(cardPoints.begin() + left, cardPoints.begin() + right + 1, 0), res = curSum;
+        while (right < n - 1)
+        {
+            ++left;
+            ++right;
+            curSum = curSum - cardPoints[left - 1] + cardPoints[right];
+            if (curSum < res) res = curSum;
+        }
+        return sum - res;
+    }
+    //贪心算法思路
+    int maxScoreV2(vector<int>& cardPoints, int k) {
+        if (k == cardPoints.size()) return accumulate(cardPoints.begin(), cardPoints.end(), 0);
+        int sumK = accumulate(cardPoints.begin(), cardPoints.begin() + k, 0), res = sumK;
+        int n = cardPoints.size();
+        for (int i = n - 1; i >= n - k; i--)
+        {
+            sumK = sumK + cardPoints[i] - cardPoints[i - n + k];
+            if (sumK > res) res = sumK;
+        }
+        return res;
+    }
+};
