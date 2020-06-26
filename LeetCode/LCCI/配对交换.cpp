f397e11284f085f44827d033ca79c3572ba992d0
Jim Li <lij42@airproducts.com>
2020-06-26 15:48:01 +0800

    面试题 05.07. 配对交换

---
+++ LeetCode/LCCI/配对交换.cpp
@@ -0,0 +1,10 @@
+﻿/*
+面试题 05.07. 配对交换
+https://leetcode-cn.com/problems/exchange-lcci/
+*/
+class Solution {
+public:
+    int exchangeBits(int num) {
+        return (num & 0xaaaaaaaa >> 1) | (num & 0x55555555 << 1);
+    }
+};
