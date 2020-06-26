b70e85a1cee75d4beccb49524973fe9cf9a2d515
Jim Li <lij42@airproducts.com>
2020-06-26 15:29:47 +0800

    面试题 16.07. 最大数值 389. 找不同

---
+++ LeetCode/00389_找不同.cpp
@@ -0,0 +1,15 @@
+﻿/*
+389. 找不同
+https://leetcode-cn.com/problems/find-the-difference/
+*/
+class Solution
+{
+    public:
+    char findTheDifference(string s, string t)
+    {
+        int res = 0;
+        for (auto c : s) res ^= c;
+        for (auto c : t) res ^= c;
+        return res;
+    }
+};
---
+++ LeetCode/LCCI/最大数值.cpp
@@ -0,0 +1,13 @@
+﻿/*
+面试题 16.07. 最大数值
+https://leetcode-cn.com/problems/maximum-lcci/
+*/
+class Solution {
+public:
+    int maximum(int a, int b) {
+        long c = a;
+        long d = b;
+        int res = (int)((fabs(c - d) + c + d) / 2);
+        return res;
+    }
+};
