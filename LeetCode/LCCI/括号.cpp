7c18a6469b4b8af905d0a52efbd27267ede2dbcf
Jim Li <lij42@airproducts.com>
2020-06-27 18:04:15 +0800

    面试题 08.09. 括号

---
+++ LeetCode/LCCI/括号.cpp
@@ -0,0 +1,35 @@
+﻿/*
+面试题 08.09. 括号
+https://leetcode-cn.com/problems/bracket-lcci/
+*/
+class Solution {
+private:
+    string data;
+    vector<string> res;
+public:
+    vector<string> generateParenthesis(int n) {
+        dfs(0, 0, 0, n, (n << 1));
+        return res;
+    }
+
+    void dfs(int step, int lcount, int rcount, int countLmt, int stepLmt)
+    {
+        if (step == stepLmt)
+        {
+            res.push_back(data);
+            return;
+        }
+        if (lcount < countLmt)
+        {
+            data.push_back('(');
+            dfs(step + 1, lcount + 1, rcount, countLmt, stepLmt);
+            data.pop_back();
+        }
+        if (lcount > rcount)
+        {
+            data.push_back(')');
+            dfs(step + 1, lcount, rcount + 1, countLmt, stepLmt);
+            data.pop_back();
+        }
+    }
+};
