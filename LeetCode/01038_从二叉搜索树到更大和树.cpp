3a2bc592118d099474f3b0cd0bbc250f5c0bae6a
Jim Li <lij42@airproducts.com>
2020-06-27 19:00:05 +0800

    1038. 从二叉搜索树到更大和树

---
+++ LeetCode/01038_从二叉搜索树到更大和树.cpp
@@ -0,0 +1,31 @@
+﻿/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+ * };
+ */
+
+/*
+1038. 从二叉搜索树到更大和树
+https://leetcode-cn.com/problems/binary-search-tree-to-greater-sum-tree/
+*/
+class Solution {
+public:
+    TreeNode* bstToGst(TreeNode* root) {
+        int sum = 0;
+        helper(root, sum);
+        return root;
+    }
+
+    void helper(TreeNode* node, int& sum)
+    {
+        if (node == nullptr) return;
+        helper(node->right, sum);
+        sum += node->val;
+        node->val = sum;
+        helper(node->left, sum);
+    }
+};
