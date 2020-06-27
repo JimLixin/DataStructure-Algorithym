5c014f6598d1e09a30caa2203e7dfd985d4af638
Jim Li <lij42@airproducts.com>
2020-06-27 18:36:47 +0800

    797. 所有可能的路径

---
+++ LeetCode/00797_所有可能的路径.cpp
@@ -0,0 +1,30 @@
+﻿/*
+797. 所有可能的路径
+https://leetcode-cn.com/problems/all-paths-from-source-to-target/
+*/
+class Solution {
+private:
+    vector<int> data;
+    vector<vector<int>> res;
+public:
+    vector<vector<int>> allPathsSourceTarget(vector<vector<int>>& graph) {
+        data.push_back(0);
+        dfs(graph, 0);
+        return res;
+    }
+
+    void dfs(vector<vector<int>>& graph, int index)
+    {
+        if (graph[index].empty())
+        {
+            res.push_back(data);
+            return;
+        }
+        for (auto n : graph[index])
+        {
+            data.push_back(n);
+            dfs(graph, n);
+            data.pop_back();
+        }
+    }
+};
