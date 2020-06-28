2b6af4b38cfd2eacaf94bd4ee543e2cd68f239e5
Jim Li <lij42@airproducts.com>
2020-06-28 19:27:40 +0800

    1051. 高度检查器

---
+++ LeetCode/01051_高度检查器.cpp
@@ -0,0 +1,17 @@
+﻿/*
+1051. 高度检查器
+https://leetcode-cn.com/problems/height-checker/
+*/
+class Solution {
+public:
+    int heightChecker(vector<int>& heights) {
+        int res = 0;
+        vector<int> data(heights.begin(), heights.end());
+        sort(data.begin(), data.end());
+        for (int i = 0; i < data.size(); i++)
+        {
+            if (data[i] != heights[i]) res++;
+        }
+        return res;
+    }
+};
