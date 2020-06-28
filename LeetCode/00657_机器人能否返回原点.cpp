6ffa30bf7d0f32f260f348ff88ad98e4aef19e35
Jim Li <lij42@airproducts.com>
2020-06-28 18:52:49 +0800

    657. 机器人能否返回原点

---
+++ LeetCode/00657_机器人能否返回原点.cpp
@@ -0,0 +1,22 @@
+﻿/*
+657. 机器人能否返回原点
+https://leetcode-cn.com/problems/robot-return-to-origin/
+*/
+class Solution {
+public:
+    bool judgeCircle(string moves) {
+        int x = 0, y = 0;
+        for (auto c : moves)
+        {
+            if (c == 'U')
+                y++;
+            else if (c == 'D')
+                y--;
+            else if (c == 'L')
+                x--;
+            else
+                x++;
+        }
+        return x == 0 && y == 0;
+    }
+};
