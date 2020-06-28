5df63a88244cd8d87301f2f47b0a487d8aa555d3
Jim Li <lij42@airproducts.com>
2020-06-28 19:20:17 +0800

    728. 自除数

---
+++ LeetCode/00728_自除数.cpp
@@ -0,0 +1,26 @@
+﻿/*
+728. 自除数
+https://leetcode-cn.com/problems/self-dividing-numbers/
+*/
+class Solution {
+public:
+    vector<int> selfDividingNumbers(int left, int right) {
+        vector<int> res;
+        for (int n = left; n <= right; n++)
+        {
+            int tmp = n;
+            bool flag = true;
+            while (tmp != 0)
+            {
+                if (tmp % 10 == 0 || n % (tmp % 10) != 0)
+                {
+                    flag = false;
+                    break;
+                }
+                tmp /= 10;
+            }
+            if (flag) res.push_back(n);
+        }
+        return res;
+    }
+};
