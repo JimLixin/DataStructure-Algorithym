df6f10952fc4ae478c658763863329841a2e17a3
Jim Li <lij42@airproducts.com>
2020-06-28 09:55:19 +0800

    209. 长度最小的子数组

---
+++ LeetCode/滑动窗口/长度最小的子数组.cpp
@@ -0,0 +1,23 @@
+﻿/*
+209. 长度最小的子数组
+https://leetcode-cn.com/problems/minimum-size-subarray-sum/
+*/
+class Solution {
+public:
+    int minSubArrayLen(int s, vector<int>& nums) {
+        if (nums.empty()) return 0;
+        int left = 0, right = 0, sum = 0, res = INT_MAX;
+        while (right < nums.size())
+        {
+            sum += nums[right];
+            right++;
+            while (sum >= s)
+            {
+                res = min(res, right - left);
+                sum -= nums[left];
+                left++;
+            }
+        }
+        return res == INT_MAX ? 0 : res;
+    }
+};
