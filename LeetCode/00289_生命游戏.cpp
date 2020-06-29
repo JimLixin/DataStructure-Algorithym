f36b26a0b6a76638e5b217c4e031227479b3c035
Jim Li <lij42@airproducts.com>
2020-06-29 15:58:01 +0800

    701. 二叉搜索树中的插入操作

---
+++ LeetCode/00289_生命游戏.cpp
@@ -0,0 +1,10 @@
+﻿/*
+生命游戏在线演示：http://home.ustc.edu.cn/~zzzz/lifegame/lifegame.html?x=120&y=100&d=30&s=60
+
+*/
+class Solution {
+public:
+    void gameOfLife(vector<vector<int>>& board) {
+
+    }
+};
---
+++ LeetCode/00701_二叉搜索树中的插入操作.cpp
@@ -0,0 +1,27 @@
+﻿/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+
+ /*
+ 701. 二叉搜索树中的插入操作
+ https://leetcode-cn.com/problems/insert-into-a-binary-search-tree/
+ */
+class Solution {
+public:
+    TreeNode* insertIntoBST(TreeNode* root, int val) {
+        if (root == nullptr) return new TreeNode(val);
+        if (root->val > val)
+            root->left = insertIntoBST(root->left, val);
+        else
+            root->right = insertIntoBST(root->right, val);
+        return root;
+    }
+};
