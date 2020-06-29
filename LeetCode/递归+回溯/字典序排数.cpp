8689fe0c020bbae838b59016d8b6973bda73123e
Jim Li <lij42@airproducts.com>
2020-06-29 19:16:28 +0800

    386. 字典序排数

---
+++ LeetCode/递归+回溯/字典序排数.cpp
@@ -0,0 +1,26 @@
+﻿/*
+386. 字典序排数
+https://leetcode-cn.com/problems/lexicographical-numbers/
+*/
+class Solution {
+public:
+    vector<int> lexicalOrder(int n) {
+        vector<int> res;
+        dfs(res, n, 0);
+        return res;
+    }
+
+    void dfs(vector<int>& res, int N, int val)
+    {
+        if (val > N) return;
+        int s = 0;
+        for (int i = 0; i < 10; i++)
+        {
+            s = i + val;
+            if (s == 0) continue;
+            if (s > N) break;
+            res.push_back(s);
+            dfs(res, N, s * 10);
+        }
+    }
+};
