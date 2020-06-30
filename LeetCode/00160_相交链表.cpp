28f1aeb6fc462c4594a255e6989683b3fcdc5f1b
Jim Li <lij42@airproducts.com>
2020-06-30 10:18:15 +0800

    160. 相交链表

---
+++ LeetCode/00160_相交链表.cpp
@@ -0,0 +1,26 @@
+﻿/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode(int x) : val(x), next(NULL) {}
+ * };
+ */
+
+/*
+160. 相交链表
+https://leetcode-cn.com/problems/intersection-of-two-linked-lists/
+*/
+class Solution {
+public:
+    ListNode* getIntersectionNode(ListNode* headA, ListNode* headB) {
+        if (!headA || !headB) return nullptr;
+        ListNode* a = headA, * b = headB;
+        while (a != b)
+        {
+            a = a ? a->next : headB;
+            b = b ? b->next : headA;
+        }
+        return a;
+    }
+};
