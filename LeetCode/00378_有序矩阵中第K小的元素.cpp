01dd29c41260d437482ff683ff3dd4062deae122
Jim Li <lij42@airproducts.com>
2020-07-02 10:40:20 +0800

    378. 有序矩阵中第K小的元素

---
+++ LeetCode/00378_有序矩阵中第K小的元素.cpp
@@ -0,0 +1,26 @@
+﻿/*
+378. 有序矩阵中第K小的元素
+https://leetcode-cn.com/problems/kth-smallest-element-in-a-sorted-matrix/
+*/
+class Solution {
+public:
+    int kthSmallest(vector<vector<int>>& matrix, int k) {
+        int res = 0;
+        while (k > 0)
+        {
+            int min = INT_MAX, index = -1;
+            for (int i = 0; i < matrix.size(); i++)
+            {
+                if (!matrix[i].empty() && matrix[i].front() < min)
+                {
+                    min = matrix[i].front();
+                    index = i;
+                }
+            }
+            res = min;
+            matrix[index].erase(matrix[index].begin());
+            k--;
+        }
+        return res;
+    }
+};
