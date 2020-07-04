1f7050322af95aaf3a5b96832d9623b68a94bea6
Jim Li <lij42@airproducts.com>
2020-07-04 20:33:06 +0800

    1402. 做菜顺序

--- LeetCode/00032_最长有效括号.cs
+++ LeetCode/00032_最长有效括号.cs
@@ -31,3 +31,35 @@ namespace Algorithym.LeetCode
         }
     }
 }
+
+/* C++版本
+class Solution {
+public:
+    int longestValidParentheses(string s) {
+        //dp[i] - s中以位置i-1结尾的字符串的有效括号长度
+        //如果 i-1位置为'(', 则当前没有可以匹配的括号对, dp[i] = 0
+        //如果 i-1位置为')', 则要看i-2位置处的情况：
+        //如果 i-2位置为'(', 说明i-1,i-2是一对匹配的括号, dp[i] = dp[i-2] + 2
+        //如果 i-2位置为')', 有可能是多对括号嵌套，需要去找前一个能跟i-1匹配上的'('的位置k, 
+        //而这个'('跟i-1中间可能存在着另一个有效的括号串，即为子问题dp[i-1], 
+        //k = i - dp[i-1] - 2就是我们需要检查是否能跟i-1匹配的那个位置 ,看k位置处是否为'('， 
+        //是则dp[i] = dp[i-1] + 2 + dp[k], dp[k]也有可能不存在，比如s是以嵌套括号对开始的。
+        int n = s.size(), ans = 0;
+        vector<int> dp(n + 1);
+        for(int i = 2; i <= n; i++)
+        {
+            if(s[i-1] == '(') continue;
+            if(s[i-2] == '(')
+                dp[i] = dp[i-2] + 2;
+            else
+            {
+                int k = i - dp[i-1] - 2;
+                if(k >= 0 && s[k] == '(') 
+                    dp[i] = dp[i-1] + 2 + (k > 0 ? dp[k] : 0);
+            }
+            ans = max(ans, dp[i]);
+        }
+        return ans;
+    }
+}; 
+*/
---
+++ LeetCode/01402_做菜顺序.cpp
@@ -0,0 +1,57 @@
+﻿/*
+1402. 做菜顺序
+https://leetcode-cn.com/problems/reducing-dishes/
+*/
+class Solution {
+public:
+    //动态规划思路
+    int maxSatisfaction(vector<int>& satisfaction) {
+        //dp[i][j] - 前i个元素中选中j个元素可以达到的最大值
+        //dp[i][j]: 对于第i个元素, 如果我们选中它，那我们需要知道前面i-1个元素中选中j-1个元素可以达到的最大值， 
+        //即dp[i][j] = dp[i-1][j-1] + satisfaction[j]
+        //如果我们不选它， 则 dp[i][j] = dp[i-1][j]
+        //base case: 
+        // j = 0:意味着我们从i个元素中选取0个，最大值只会为0，即dp[i][0] = 0
+        // i == j, 我们从前i个元素中选择全部i个得到的最大值，就是 dp[i-1][j-1] + satisfaction[i]
+        int n = satisfaction.size(), ans = 0;
+        sort(satisfaction.begin(), satisfaction.end());
+        vector<vector<int>> dp(n + 1, vector<int>(n + 1));
+        dp[1][1] = satisfaction[0];
+        for (int i = 2; i <= n; i++)
+        {
+            for (int j = 0; j <= i; j++)
+            {
+                if (j == 0) dp[i][j] = 0;
+                else
+                {
+                    if (i == j)
+                        dp[i][j] = dp[i - 1][j - 1] + satisfaction[i - 1] * j;
+                    else
+                        dp[i][j] = max(dp[i - 1][j - 1] + satisfaction[i - 1] * j, dp[i - 1][j]);
+                    ans = max(ans, dp[i][j]);
+                }
+            }
+        }
+        return ans;
+    }
+
+    //贪心算法思路
+    int maxSatisfactionV2(vector<int>& satisfaction) {
+        int n = satisfaction.size(), sum = 0, ans = 0;
+        vector<int> preSum(n + 1);
+        sort(satisfaction.begin(), satisfaction.end());
+        for (int i = 1; i <= n; i++)
+        {
+            preSum[i] = preSum[i - 1] + satisfaction[i - 1];
+            sum += satisfaction[i - 1] * i;
+        }
+        ans = sum;
+        for (int i = 0; i < n; i++)
+        {
+            if (satisfaction[i] >= 0) break;
+            sum -= preSum[n] - preSum[i];
+            ans = max(ans, sum);
+        }
+        return ans;
+    }
+};
