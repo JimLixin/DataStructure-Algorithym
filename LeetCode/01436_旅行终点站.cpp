59ea012fe12deaa742e8d021911d343d2d3f8e7c
Jim Li <lij42@airproducts.com>
2020-07-06 19:21:46 +0800

    1436. 旅行终点站

---
+++ LeetCode/01436_旅行终点站.cpp
@@ -0,0 +1,17 @@
+﻿/*
+1436. 旅行终点站
+https://leetcode-cn.com/problems/destination-city/
+*/
+class Solution {
+public:
+    string destCity(vector<vector<string>>& paths) {
+        unordered_map<string, string> map;
+        for (auto p : paths) map[p[0]] = p[1];
+        for (auto iter = map.begin(); iter != map.end(); ++iter)
+        {
+            if (map.count(iter->second) == 0)
+                return iter->second;
+        }
+        return nullptr;
+    }
+};
