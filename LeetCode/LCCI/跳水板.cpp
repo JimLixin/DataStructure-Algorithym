32feb8c7e0a19640eabb403e790f5f5df9de4f22
Jim Li <lij42@airproducts.com>
2020-07-08 19:22:23 +0800

    面试题 16.11. 跳水板

---
+++ LeetCode/LCCI/跳水板.cpp
@@ -0,0 +1,21 @@
+﻿/*
+面试题 16.11. 跳水板
+https://leetcode-cn.com/problems/diving-board-lcci/
+*/
+class Solution {
+public:
+    vector<int> divingBoard(int shorter, int longer, int k) {
+        if (k == 0)
+            return vector<int>{};
+        unordered_set<int> set;
+        for (int i = 0; i <= k; ++i)
+        {
+            int j = k - i;
+            set.insert(shorter * i + longer * j);
+        }
+        vector<int> res(set.begin(), set.end());
+        sort(res.begin(), res.end());
+        return res;
+    }
+
+};
