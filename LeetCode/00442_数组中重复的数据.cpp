aba8cfbb97c70d1cec678e5a06627f51f63e656c
Jim Li <lij42@airproducts.com>
2020-07-16 19:30:37 +0800

    442. 数组中重复的数据

---
+++ LeetCode/00442_数组中重复的数据.cpp
@@ -0,0 +1,20 @@
+﻿/*
+442. 数组中重复的数据
+https://leetcode-cn.com/problems/find-all-duplicates-in-an-array/
+*/
+class Solution {
+public:
+    //时间复杂度: O(N)
+    //空间复杂度: O(N)
+    vector<int> findDuplicates(vector<int>& nums) {
+        int n = nums.size();
+        vector<int> data(n);
+        vector<int> ans;
+        for (int i = 0; i < n; ++i) data[nums[i] - 1]++;
+        for (int i = 0; i < n; ++i)
+        {
+            if (data[i] > 1) ans.push_back(i + 1);
+        }
+        return ans;
+    }
+};
---
+++ LeetCode/315_计算右侧小于当前元素的个数.cpp
@@ -0,0 +1,20 @@
+﻿/*
+315. 计算右侧小于当前元素的个数
+https://leetcode-cn.com/problems/count-of-smaller-numbers-after-self/
+*/
+class Solution {
+public:
+    vector<int> countSmaller(vector<int>& nums) {
+        int n = nums.size();
+        vector<int> ans(n);
+        for (int i = 0; i < n; i++)
+        {
+            for (int j = i + 1; j < n; j++)
+            {
+                if (nums[j] < nums[i])
+                    ans[i]++;
+            }
+        }
+        return ans;
+    }
+};
