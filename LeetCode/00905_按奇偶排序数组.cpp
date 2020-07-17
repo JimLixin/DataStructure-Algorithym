c1e3ba3cff5a91fe5071d897876daae8715b4818
Jim Li <lij42@airproducts.com>
2020-07-17 18:32:01 +0800

    905. 按奇偶排序数组

---
+++ LeetCode/00905_按奇偶排序数组.cpp
@@ -0,0 +1,20 @@
+﻿/*
+905. 按奇偶排序数组
+https://leetcode-cn.com/problems/sort-array-by-parity/
+*/
+class Solution {
+public:
+    vector<int> sortArrayByParity(vector<int>& A) {
+        int left = 0, right = A.size() - 1;
+        while (left < right)
+        {
+            while (left < right && (A[left] & 1) == 0) left++;
+            while (left < right && (A[right] & 1) > 0) right--;
+            if (left >= right) break;
+            A[left] = A[left] ^ A[right];
+            A[right] = A[left] ^ A[right];
+            A[left] = A[left] ^ A[right];
+        }
+        return A;
+    }
+};
